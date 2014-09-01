56cda9c2bac19d8c8646da557eb20c25f2eacc45
Sriram Subramanian <sriram.sub@gmail.com>
2014-08-31 21:01:18 -0700

    Added basic test for replica metadata request and some logging to debug a bug

--- ambry-shared/src/main/java/com.github.ambry.shared/ReplicaMetadataRequest.java
+++ ambry-shared/src/main/java/com.github.ambry.shared/ReplicaMetadataRequest.java
@@ -10,6 +10,8 @@ import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
 import java.util.ArrayList;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
@@ -19,6 +21,7 @@ public class ReplicaMetadataRequest extends RequestOrResponse {
   private List<ReplicaMetadataRequestInfo> replicaMetadataRequestInfoList;
   private long maxTotalSizeOfEntriesInBytes;
   private long replicaMetadataRequestInfoListSizeInBytes;
+  private final Logger logger = LoggerFactory.getLogger(getClass());
 
   private static final int Max_Entries_Size_In_Bytes = 8;
   private static final int Replica_Metadata_Request_Info_List_Size_In_Bytes = 4;
@@ -26,6 +29,9 @@ public class ReplicaMetadataRequest extends RequestOrResponse {
   public ReplicaMetadataRequest(int correlationId, String clientId,
       List<ReplicaMetadataRequestInfo> replicaMetadataRequestInfoList, long maxTotalSizeOfEntriesInBytes) {
     super(RequestOrResponseType.ReplicaMetadataRequest, Request_Response_Version, correlationId, clientId);
+    if (replicaMetadataRequestInfoList == null) {
+      throw new IllegalArgumentException("replicaMetadataRequestInfoList cannot be null");
+    }
     this.replicaMetadataRequestInfoList = replicaMetadataRequestInfoList;
     this.maxTotalSizeOfEntriesInBytes = maxTotalSizeOfEntriesInBytes;
     this.replicaMetadataRequestInfoListSizeInBytes = 0;
@@ -37,10 +43,16 @@ public class ReplicaMetadataRequest extends RequestOrResponse {
   public static ReplicaMetadataRequest readFrom(DataInputStream stream, ClusterMap clusterMap, FindTokenFactory factory)
       throws IOException {
     RequestOrResponseType type = RequestOrResponseType.ReplicaMetadataRequest;
+    Logger logger = LoggerFactory.getLogger(ReplicaMetadataRequest.class);
     Short versionId = stream.readShort();
+    logger.info("version id from replica metadata request " + versionId);
     int correlationId = stream.readInt();
+    logger.info("correlation id from replica metadata request " + correlationId);
     String clientId = Utils.readIntString(stream);
+    logger.info("client id from replica metadata request " + clientId);
     int replicaMetadataRequestInfoListCount = stream.readInt();
+    logger.info("replicaMetadataRequestInfoListCount from replica metadata request " +
+        replicaMetadataRequestInfoListCount);
     ArrayList<ReplicaMetadataRequestInfo> replicaMetadataRequestInfoList =
         new ArrayList<ReplicaMetadataRequestInfo>(replicaMetadataRequestInfoListCount);
     for (int i = 0; i < replicaMetadataRequestInfoListCount; i++) {
@@ -49,6 +61,7 @@ public class ReplicaMetadataRequest extends RequestOrResponse {
       replicaMetadataRequestInfoList.add(replicaMetadataRequestInfo);
     }
     long maxTotalSizeOfEntries = stream.readLong();
+    logger.info("maxTotalSizeOfEntries from replica metadata request " + maxTotalSizeOfEntries);
     // ignore version for now
     return new ReplicaMetadataRequest(correlationId, clientId, replicaMetadataRequestInfoList, maxTotalSizeOfEntries);
   }
@@ -68,10 +81,13 @@ public class ReplicaMetadataRequest extends RequestOrResponse {
       bufferToSend = ByteBuffer.allocate((int) sizeInBytes());
       writeHeader();
       bufferToSend.putInt(replicaMetadataRequestInfoList.size());
+      logger.info("correlation id for replica metadata request " + correlationId);
+      logger.info("Size put for replica metadata request " + replicaMetadataRequestInfoList.size());
       for (ReplicaMetadataRequestInfo replicaMetadataRequestInfo : replicaMetadataRequestInfoList) {
         replicaMetadataRequestInfo.writeTo(bufferToSend);
       }
       bufferToSend.putLong(maxTotalSizeOfEntriesInBytes);
+      logger.info("maxTotalSizeOfEntriesInBytes put for replica metadata request " + maxTotalSizeOfEntriesInBytes);
       bufferToSend.flip();
     }
     if (bufferToSend.remaining() > 0) {
