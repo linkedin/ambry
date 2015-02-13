e702b9a3cd55f47680fec8efdf22168ddfebdb08
Sriram Subramanian <sriram.sub@gmail.com>
2015-02-13 00:43:10 -0800

    1. Add test for BitUtils 2. Move SocketServerTest to network and make it more generic

---
+++ ambry-network/src/test/java/com.github.ambry.network/SocketServerTest.java
@@ -0,0 +1,95 @@
+package com.github.ambry.network;
+
+import com.codahale.metrics.MetricRegistry;
+import com.github.ambry.config.NetworkConfig;
+import com.github.ambry.config.VerifiableProperties;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.Random;
+
+
+public class SocketServerTest {
+
+  private SocketServer server = null;
+
+  public SocketServerTest()
+      throws InterruptedException, IOException {
+    Properties props = new Properties();
+    VerifiableProperties propverify = new VerifiableProperties(props);
+    NetworkConfig config = new NetworkConfig(propverify);
+    server = new SocketServer(config, new MetricRegistry());
+    server.start();
+  }
+
+  @After
+  public void cleanup() {
+    server.shutdown();
+  }
+
+  @Test
+  public void simpleRequest()
+      throws IOException, InterruptedException {
+    byte[] bytesToSend = new byte[1028];
+    new Random().nextBytes(bytesToSend);
+    ByteBuffer byteBufferToSend = ByteBuffer.wrap(bytesToSend);
+    byteBufferToSend.putLong(0, 1028);
+    BoundedByteBufferSend bufferToSend = new BoundedByteBufferSend(byteBufferToSend);
+    BlockingChannel channel = new BlockingChannel("localhost", server.getPort(), 10000, 10000, 1000, 2000);
+    channel.connect();
+    channel.send(bufferToSend);
+    RequestResponseChannel requestResponseChannel = server.getRequestResponseChannel();
+    Request request = requestResponseChannel.receiveRequest();
+    DataInputStream requestStream = new DataInputStream(request.getInputStream());
+    byte[] outputBytes = new byte[1020];
+    requestStream.readFully(outputBytes);
+    for (int i = 0; i < 1020; i++) {
+      Assert.assertEquals(bytesToSend[8 + i], outputBytes[i]);
+    }
+
+    // send response back and ensure response is received
+    byte[] responseBytes = new byte[2048];
+    new Random().nextBytes(responseBytes);
+    ByteBuffer byteBufferToSendResponse = ByteBuffer.wrap(responseBytes);
+    byteBufferToSendResponse.putLong(0, 2048);
+    BoundedByteBufferSend responseToSend = new BoundedByteBufferSend(byteBufferToSendResponse);
+    requestResponseChannel.sendResponse(responseToSend, request, null);
+    InputStream streamResponse = channel.receive().getInputStream();
+    byte[] responseBytesReceived = new byte[2040];
+    streamResponse.read(responseBytesReceived);
+    for (int i = 0; i < 2040; i++) {
+      Assert.assertEquals(responseBytes[8 + i], responseBytesReceived[i]);
+    }
+    channel.disconnect();
+  }
+
+  /**
+   * Choose a number of random available ports
+   */
+  ArrayList<Integer> choosePorts(int count)
+      throws IOException {
+    ArrayList<Integer> sockets = new ArrayList<Integer>();
+    for (int i = 0; i < count; i++) {
+      ServerSocket socket = new ServerSocket(0);
+      sockets.add(socket.getLocalPort());
+      socket.close();
+    }
+    return sockets;
+  }
+
+  /**
+   * Choose an available port
+   */
+  public int choosePort()
+      throws IOException {
+    return choosePorts(1).get(0);
+  }
+}
--- ambry-utils/src/test/java/com.github.ambry.utils/BitUtilTest.java
+++ ambry-utils/src/test/java/com.github.ambry.utils/BitUtilTest.java
@@ -1,7 +1,36 @@
 package com.github.ambry.utils;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
+
 /**
  * Test for all the bit manipulation utils
  */
 public class BitUtilTest {
+  @Test
+  public void testBitUtil() {
+    Assert.assertTrue(BitUtil.isPowerOfTwo(64));
+    Assert.assertFalse(BitUtil.isPowerOfTwo(63));
+    Assert.assertTrue(BitUtil.isPowerOfTwo(4096L));
+    Assert.assertFalse(BitUtil.isPowerOfTwo(4097L));
+    Assert.assertEquals(BitUtil.nextHighestPowerOfTwo(62), 64);
+    Assert.assertEquals(BitUtil.nextHighestPowerOfTwo(4092L), 4096);
+    Assert.assertEquals(BitUtil.ntz(36), 2);
+    Assert.assertEquals(BitUtil.ntz(4096L), 12);
+    Assert.assertEquals(BitUtil.ntz2(4096L), 12);
+    Assert.assertEquals(BitUtil.ntz3(4096L), 12);
+    Assert.assertEquals(BitUtil.pop(4096), 1);
+    long[] words1 = new long[2];
+    words1[0] = 1266;
+    words1[1] = 1876;
+    long[] words2 = new long[2];
+    words2[0] = 7654;
+    words2[1] = 2567;
+    Assert.assertEquals(BitUtil.pop_andnot(words1, words2, 0, 2), 5);
+    Assert.assertEquals(BitUtil.pop_array(words1, 0, 2), 12);
+    Assert.assertEquals(BitUtil.pop_intersect(words1, words2, 0, 2), 7);
+    Assert.assertEquals(BitUtil.pop_union(words1, words2, 0, 2), 19);
+    Assert.assertEquals(BitUtil.pop_xor(words1, words2, 0, 2), 12);
+  }
 }
