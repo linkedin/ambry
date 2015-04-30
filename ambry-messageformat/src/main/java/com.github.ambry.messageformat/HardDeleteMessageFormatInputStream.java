a452fd4a3071f2c3489e0e001afd2e9eeabbb9d3
Priyesh Narayanan <priyesh@gmail.com>
2015-04-30 13:14:27 -0700

    More

--- ambry-messageformat/src/main/java/com.github.ambry.messageformat/HardDeleteMessageFormatInputStream.java
+++ ambry-messageformat/src/main/java/com.github.ambry.messageformat/HardDeleteMessageFormatInputStream.java
@@ -1,8 +1,6 @@
 package com.github.ambry.messageformat;
 
 import com.github.ambry.store.StoreKey;
-import com.github.ambry.utils.Crc32;
-import com.github.ambry.utils.CrcInputStream;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 
@@ -24,36 +22,11 @@ import java.nio.ByteBuffer;
  * |       Blob Record (Zeroed out)      |
  *  - - - - - - - - - - - - - - - - - - -
  */
-public class HardDeleteMessageFormatInputStream extends MessageFormatInputStream {
+public class HardDeleteMessageFormatInputStream extends PutMessageFormatInputStream {
 
   public HardDeleteMessageFormatInputStream(StoreKey key, BlobProperties blobProperties, ByteBuffer userMetadata,
       InputStream data, long streamSize)
       throws MessageFormatException {
-
-    int headerSize = MessageFormatRecord.MessageHeader_Format_V1.getHeaderSize();
-    int blobPropertiesRecordSize =
-        MessageFormatRecord.BlobProperties_Format_V1.getBlobPropertiesRecordSize(blobProperties);
-    int userMetadataSize = MessageFormatRecord.UserMetadata_Format_V1.getUserMetadataSize(userMetadata);
-    long blobSize = MessageFormatRecord.Blob_Format_V1.getBlobRecordSize(streamSize);
-
-    buffer = ByteBuffer.allocate(headerSize + key.sizeInBytes() + blobPropertiesRecordSize + userMetadataSize +
-        (int) (blobSize - streamSize - MessageFormatRecord.Crc_Size));
-
-    MessageFormatRecord.MessageHeader_Format_V1
-        .serializeHeader(buffer, blobPropertiesRecordSize + userMetadataSize + blobSize, headerSize + key.sizeInBytes(),
-            MessageFormatRecord.Message_Header_Invalid_Relative_Offset,
-            headerSize + key.sizeInBytes() + blobPropertiesRecordSize,
-            headerSize + key.sizeInBytes() + blobPropertiesRecordSize + userMetadataSize);
-    buffer.put(key.toBytes());
-    MessageFormatRecord.BlobProperties_Format_V1.serializeBlobPropertiesRecord(buffer, blobProperties);
-    MessageFormatRecord.UserMetadata_Format_V1.serializeUserMetadataRecord(buffer, userMetadata);
-    int bufferBlobStart = buffer.position();
-    MessageFormatRecord.Blob_Format_V1.serializePartialBlobRecord(buffer, streamSize);
-    Crc32 crc = new Crc32();
-    crc.update(buffer.array(), bufferBlobStart, buffer.position() - bufferBlobStart);
-    stream = new CrcInputStream(crc, data);
-    streamLength = streamSize;
-    messageLength = buffer.capacity() + streamLength + MessageFormatRecord.Crc_Size;
-    buffer.flip();
+    super(key, blobProperties, userMetadata, data, streamSize);
   }
 }
