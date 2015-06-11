3e3753d0a0c447d308539144b8273c1ff92e6f2b
sinaraya <n.siva.b@gmail.com>
2015-06-11 12:21:51 -0700

    Adding java doc for utils

--- ambry-utils/src/main/java/com.github.ambry.utils/Utils.java
+++ ambry-utils/src/main/java/com.github.ambry.utils/Utils.java
@@ -520,6 +520,13 @@ public class Utils {
     return epochTimeInMs + (deltaTimeInSeconds * Time.MsPerSec);
   }
 
+  /**
+   * Read "size" bytes from stream to a byte array
+   * @param stream from which data to be read from
+   * @param size length of bytes to be read from the stream
+   * @return byte[] which has the data that is read from the stream
+   * @throws IOException
+   */
   public static byte[] readBytesFromStream(DataInputStream stream, int size)
       throws IOException {
     byte[] outputBytes = new byte[size];
@@ -527,6 +534,14 @@ public class Utils {
     return outputBytes;
   }
 
+  /**
+   * Read "size" bytes from stream to a byte array at the given offset
+   * @param stream from which data to be read from
+   * @param data byte[] into which the data has to be written
+   * @param offset starting offset in the byte[] at which the data has to be written to
+   * @param size length of bytes to be read from the stream
+   * @throws IOException
+   */
   public static void readBytesFromStream(InputStream stream, byte[] data, int offset, int size)
       throws IOException {
     int read = 0;
