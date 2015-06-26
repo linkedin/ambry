d4189a8a9a4967ff99b96cf3032e0dfd0019ef69
sinaraya <n.siva.b@gmail.com>
2015-06-26 10:05:23 -0700

    fixing logging

--- ambry-replication/src/main/java/com.github.ambry.replication/ReplicaThread.java
+++ ambry-replication/src/main/java/com.github.ambry.replication/ReplicaThread.java
@@ -631,9 +631,9 @@ class ReplicaThread implements Runnable {
                         metricRegistry);
                 if (validMessageDetectionInputStream.hasInvalidMessages()) {
                   replicationMetrics.incrementInvalidMessageError(partitionResponseInfo.getPartition());
-                  logger.error(
+                  logger.error("Out of " + (messageInfoList.size()) + " messages, " +
                       (messageInfoList.size() - validMessageDetectionInputStream.getValidMessageInfoList().size())
-                          + " invalid messages found in message stream from " + remoteReplicaInfo.getReplicaId());
+                      + " invalid messages were found in message stream from " + remoteReplicaInfo.getReplicaId());
                 }
                 messageInfoList = validMessageDetectionInputStream.getValidMessageInfoList();
                 if (messageInfoList.size() == 0) {
