1cc7f96dcce05faece95a8ecece6cbc3c25fe906
Tianwei Li <tili@tili-mn1.linkedin.biz>
2015-08-11 00:23:42 -0700

    add more asserts in SSLFactoryTest

--- ambry-network/src/test/java/com.github.ambry.network/SSLFactoryTest.java
+++ ambry-network/src/test/java/com.github.ambry.network/SSLFactoryTest.java
@@ -5,6 +5,7 @@ import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocketFactory;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -31,6 +32,17 @@ public class SSLFactoryTest {
       SSLSocketFactory socketFactory = sslContext.getSocketFactory();
       SSLServerSocketFactory serverSocketFactory = sslContext.getServerSocketFactory();
       SSLEngine engine = sslFactory.createSSLEngine(sslContext, "localhost", 9095, true);
+
+      Assert.assertEquals(sslContext.getProtocol(), "TLS");
+      String[] enabledCipherSuites = engine.getEnabledCipherSuites();
+      Assert.assertEquals(enabledCipherSuites.length, 1);
+      Assert.assertEquals(enabledCipherSuites[0], "TLS_RSA_WITH_AES_128_CBC_SHA256");
+      String[] enabledProtocols = engine.getEnabledProtocols();
+      Assert.assertEquals(enabledProtocols.length, 1);
+      Assert.assertEquals(enabledProtocols[0], "TLSv1.2");
+      Assert.assertEquals(engine.getNeedClientAuth(), false);
+      Assert.assertEquals(engine.getUseClientMode(), true);
+      Assert.assertEquals(engine.getWantClientAuth(), false);
       System.out.println(socketFactory.toString());
       System.out.println(serverSocketFactory.toString());
       System.out.println(engine.toString());
