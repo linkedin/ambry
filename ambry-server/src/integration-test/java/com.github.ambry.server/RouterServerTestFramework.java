21a66c776c5f378e45e749e845e555fd4bfe078d
Casey Getz <cgetz@linkedin.com>
2016-06-03 15:48:36 -0700

    change router to test dependency and added license file

--- ambry-server/src/integration-test/java/com.github.ambry.server/RouterServerTestFramework.java
+++ ambry-server/src/integration-test/java/com.github.ambry.server/RouterServerTestFramework.java
@@ -1,6 +1,18 @@
+/**
+ * Copyright 2016 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
 package com.github.ambry.server;
 
-import com.github.ambry.clustermap.ClusterMap;
 import com.github.ambry.clustermap.MockClusterMap;
 import com.github.ambry.commons.ByteBufferAsyncWritableChannel;
 import com.github.ambry.commons.ByteBufferReadableStreamChannel;
@@ -12,7 +24,6 @@ import com.github.ambry.messageformat.BlobProperties;
 import com.github.ambry.notification.NotificationSystem;
 import com.github.ambry.router.Callback;
 import com.github.ambry.router.CoordinatorBackedRouter;
-import com.github.ambry.router.CoordinatorBackedRouterFactory;
 import com.github.ambry.router.CoordinatorBackedRouterMetrics;
 import com.github.ambry.router.NonBlockingRouterFactory;
 import com.github.ambry.router.ReadableStreamChannel;
@@ -200,7 +211,7 @@ class RouterServerTestFramework {
               try {
                 future.get();
                 Assert.fail("Blob should have been deleted in operation: " + getOperationName());
-              } catch (Exception e) {
+              } catch (Exception ignored) {
               }
             } else {
               checkBlob(get(), opInfo, getOperationName());
