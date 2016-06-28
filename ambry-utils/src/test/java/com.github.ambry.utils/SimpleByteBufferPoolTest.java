50083b7d5b9a7eb60f987f858df4916c3254eb74
Ming Xia <mxia@linkedin.com>
2016-06-28 10:42:56 -0700

    Checks blocking state when request made to SimpleBufferPool
    This patch adds an explicit check for the blocking state when a request is made to
    a SimpleBufferPool but cannot be immediately satisfied.

--- ambry-utils/src/test/java/com.github.ambry.utils/SimpleByteBufferPoolTest.java
+++ ambry-utils/src/test/java/com.github.ambry.utils/SimpleByteBufferPoolTest.java
@@ -155,6 +155,8 @@ public class SimpleByteBufferPoolTest {
     }
     assertEquals(0, pool.availableMemory());
     consumerThreads[1].start();
+    Thread.sleep(1);
+    assertEquals(Thread.State.TIMED_WAITING, consumerThreads[1].getState());
     used[0].countDown();
     consumerThreads[0].join();
     used[1].countDown();
