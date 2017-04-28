8960af05e0f076d70167ea36a5e8a4a2b399156d
sinaraya <n.siva.b@gmail.com>
2017-04-27 17:58:51 -0700

    Fix Utils#addSecondsToEpochTime()

--- ambry-utils/src/test/java/com.github.ambry.utils/UtilsTest.java
+++ ambry-utils/src/test/java/com.github.ambry.utils/UtilsTest.java
@@ -393,6 +393,23 @@ public class UtilsTest {
     assertEquals("Time in Ms to the nearest Sec mismatch ", msValue, Utils.getTimeInMsToTheNearestSec(msValue));
   }
 
+  /**
+   * Tests {@link Utils#addSecondsToEpochTime(long, long)}
+   */
+  @Test
+  public void addSecondsToEpochTimeTest() {
+    for (int i = 0; i < 5; i++) {
+      long epochTimeInMs = SystemTime.getInstance().milliseconds() + TestUtils.RANDOM.nextInt(10000);
+      long deltaInSecs = TestUtils.RANDOM.nextInt(10000);
+      assertEquals("epoch epochTimeInMs mismatch ", epochTimeInMs + (deltaInSecs * Time.MsPerSec),
+          Utils.addSecondsToEpochTime(epochTimeInMs, deltaInSecs));
+      assertEquals("epoch epochTimeInMs mismatch ", Utils.Infinite_Time,
+          Utils.addSecondsToEpochTime(Utils.Infinite_Time, deltaInSecs));
+      assertEquals("epoch epochTimeInMs mismatch ", Utils.Infinite_Time,
+          Utils.addSecondsToEpochTime(epochTimeInMs, Utils.Infinite_Time));
+    }
+  }
+
   private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   static Random random = new Random();
 
