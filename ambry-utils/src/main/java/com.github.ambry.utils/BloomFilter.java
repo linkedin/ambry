276195746ef3a0865800d1f629ae22c6c77e734c
André Laszlo <andre@laszlo.nu>
2017-05-18 22:48:58 +0200

    Fix broken url to "Building a Better Bloom Filter" (#633)

--- ambry-utils/src/main/java/com.github.ambry.utils/BloomFilter.java
+++ ambry-utils/src/main/java/com.github.ambry.utils/BloomFilter.java
@@ -34,7 +34,7 @@ public abstract class BloomFilter implements IFilter {
 
   // Murmur is faster than an SHA-based approach and provides as-good collision
   // resistance.  The combinatorial generation approach described in
-  // http://www.eecs.harvard.edu/~kirsch/pubs/bbbf/esa06.pdf
+  // https://www.eecs.harvard.edu/~michaelm/postscripts/tr-02-05.pdf
   // does prove to work in actual tests, and is obviously faster
   // than performing further iterations of murmur.
   long[] getHashBuckets(ByteBuffer b, int hashCount, long max) {
