494eb1911cdd6c927f9da374cfa6c2bb1cc2afe1
Gopalakrishna Holla <gholla@linkedin.com>
2017-05-31 15:01:53 -0700

    Adding a config to selectively enable different compaction triggers (#645)
    It might be desirable to enable/disable certain compaction triggers to get
    a better handle on the pathways through which compaction can be triggered.

--- ambry-api/src/main/java/com.github.ambry/config/StoreConfig.java
+++ ambry-api/src/main/java/com.github.ambry/config/StoreConfig.java
@@ -97,11 +97,13 @@ public class StoreConfig {
   public final long storeSegmentSizeInBytes;
 
   /**
-   * Whether compaction is to be enabled or not
+   * Comma separated list of the compaction triggers that should be enabled. If this config is an empty string,
+   * compaction will not be enabled.
+   * The valid triggers are: Periodic,Admin
    */
-  @Config("store.enable.compaction")
-  @Default("false")
-  public final boolean storeEnableCompaction;
+  @Config("store.compaction.triggers")
+  @Default("")
+  public final String[] storeCompactionTriggers;
 
   /**
    * The frequency (in hours) at which a store is checked to see whether it is ready for compaction.
@@ -153,7 +155,7 @@ public class StoreConfig {
         verifiableProperties.getLongInRange("store.segment.size.in.bytes", Long.MAX_VALUE, 1, Long.MAX_VALUE);
     storeMinUsedCapacityToTriggerCompactionInPercentage =
         verifiableProperties.getInt("store.min.used.capacity.to.trigger.compaction.in.percentage", 50);
-    storeEnableCompaction = verifiableProperties.getBoolean("store.enable.compaction", false);
+    storeCompactionTriggers = verifiableProperties.getString("store.compaction.triggers", "").split(",");
     storeCompactionCheckFrequencyInHours =
         verifiableProperties.getIntInRange("store.compaction.check.frequency.in.hours", 7 * 24, 1, 365 * 24);
     storeCompactionPolicyFactory = verifiableProperties.getString("store.compaction.policy.factory",
