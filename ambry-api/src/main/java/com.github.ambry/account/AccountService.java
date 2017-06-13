5fe2dc3d7faa7b6e7401ebf328da01e6f680ea89
Ming Xia <xiaming2006@gmail.com>
2017-06-13 16:12:31 -0700

    Adds AccountService and AccountServiceFactory interfaces.

---
+++ ambry-api/src/main/java/com.github.ambry/account/AccountService.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2017 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package com.github.ambry.account;
+
+import java.io.Closeable;
+import java.util.Collection;
+
+
+/**
+ * <p>
+ *   An {@code AccountService} is a component that can respond to queries for {@link Account} by id or name, and
+ *   add/update {@link Account} for future queries. The {@link Account}s under an {@code AccountService} cannot
+ *   have duplicate ids or names, and <em>MUST</em> have their ids and names one-to-one mapped.
+ * </p>
+ * <p>
+ *   Deleting an {@link Account} from {@code AccountService} is not currently supported.
+ * </p>
+ */
+public interface AccountService extends Closeable {
+
+  /**
+   * Gets an {@link Account} by its id.
+   * @param accountId The id of an {@link Account} to get.
+   * @return The {@link Account} with the specified id. {@code null} if such an {@link Account} does not exist.
+   */
+  public Account getAccountById(short accountId);
+
+  /**
+   * Gets an {@link Account} by its name.
+   * @param accountName The name of an {@link Account} to get.
+   * @return The {@link Account} with the specified name. {@code null} if such an {@link Account} does not exist.
+   */
+  public Account getAccountByName(String accountName);
+
+  /**
+   * <p>
+   *   Makes update for a collection of {@link Account}s. The update operation can either succeed with all the
+   *   {@link Account}s successfully updated, or fail with none of the {@link Account}s updated. Partial update
+   *   will not happen.
+   * </p>
+   * <p>
+   *   The caller needs to make sure the {@link Account}s to update do not have duplicate id, nor duplicate name,
+   *   otherwise, the update operation will fail.
+   * </p>
+   * <p>
+   *   When updating {@link Account}s, {@code AccountService} will check that there is no conflict between the
+   *   {@link Account}s to update and the existing {@link Account}s. If there is any conflict, the update operation
+   *   will not go through. Below lists the possible cases when there is conflict.
+   * </p>
+   * <pre>
+   * Existing account
+   * AccountId     AccountName
+   * 1             "a"
+   * 2             "b"
+   *
+   * Account to update
+   * Case   AccountId   AccountName   If Conflict    Treatment                    Conflict reason
+   * A      1           "a"           no             replace existing record      N/A
+   * B      1           "c"           no             replace existing record      N/A
+   * C      3           "c"           no             add a new record             N/A
+   * D      3           "a"           yes            fail update                  conflicts with existing name.
+   * E      1           "b"           yes            fail update                  conflicts with existing name.
+   * </pre>
+   * @param accounts The collection of {@link Account}s to update. Cannot be {@code null}.
+   * @return {@code true} if the update operation is successful, {@code false} otherwise.
+   */
+  public boolean updateAccounts(Collection<Account> accounts);
+
+  /**
+   * Gets all the {@link Account}s in this {@code AccountService}. The {@link Account}s <em>MUST</em> have their
+   * ids and names one-to-one mapped.
+   * @return A collection of {@link Account}s.
+   */
+  public Collection<Account> getAllAccounts();
+}
