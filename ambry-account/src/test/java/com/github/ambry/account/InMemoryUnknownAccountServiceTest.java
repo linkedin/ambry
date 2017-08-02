0146a973ef18e999ec2aa37385422be4b9208ef3
Ming Xia <xiahome@users.noreply.github.com>
2017-08-02 11:52:06 -0700

    Revert "Removes InMemoryUnknownAccountService (#705)"
    This reverts commit 26e4f6281a38916b66eb8d68d0bafb5bc2e17fab.

---
+++ ambry-account/src/test/java/com/github/ambry/account/InMemoryUnknownAccountServiceTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2017 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package com.github.ambry.account;
+
+import com.github.ambry.utils.Utils;
+import com.github.ambry.utils.UtilsTest;
+import java.util.Collections;
+import java.util.Random;
+import org.junit.After;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Unit tests for {@link InMemoryUnknownAccountService} and {@link InMemoryUnknownAccountServiceFactory}.
+ */
+public class InMemoryUnknownAccountServiceTest {
+  private static final Random random = new Random();
+  private AccountService accountService;
+
+  @After
+  public void close() throws Exception {
+    accountService.close();
+  }
+
+  @Test
+  public void testAllMethods() throws Exception {
+    accountService = new InMemoryUnknownAccountServiceFactory(null, null, null).getAccountService();
+    assertEquals("Wrong account", Account.UNKNOWN_ACCOUNT, accountService.getAccountById(Utils.getRandomShort(random)));
+    assertEquals("Wrong account", Account.UNKNOWN_ACCOUNT, accountService.getAccountById((short) -1));
+    assertEquals("Wrong account", Account.UNKNOWN_ACCOUNT,
+        accountService.getAccountByName(UtilsTest.getRandomString(10)));
+    assertEquals("Wrong size of account collection", 1, accountService.getAllAccounts().size());
+    // updating the InMemoryUnknownAccountService should fail.
+    Account account = new AccountBuilder((short) 1, "a", Account.AccountStatus.INACTIVE, null).build();
+    assertFalse("Wrong return value from an unsuccessful update operation",
+        accountService.updateAccounts(Collections.singletonList(account)));
+    assertEquals("Wrong size of account collection", 1, accountService.getAllAccounts().size());
+    try {
+      accountService.getAllAccounts().add(account);
+      fail("Should have thrown.");
+    } catch (UnsupportedOperationException e) {
+      // expected
+    }
+    accountService.close();
+  }
+
+  /**
+   * Tests {@code null} inputs.
+   */
+  @Test
+  public void testNullInputs() {
+    accountService = new InMemoryUnknownAccountServiceFactory(null, null, null).getAccountService();
+    try {
+      accountService.updateAccounts(null);
+      fail("should have thrown");
+    } catch (NullPointerException e) {
+      // expected
+    }
+    try {
+      accountService.getAccountByName(null);
+      fail("should have thrown");
+    } catch (NullPointerException e) {
+      // expected
+    }
+  }
+}
