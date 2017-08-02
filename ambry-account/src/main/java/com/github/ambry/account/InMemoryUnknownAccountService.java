8a5da165ff11e00c6334e85a3f0a99e756b44051
Ming Xia <xiahome@users.noreply.github.com>
2017-08-02 11:54:39 -0700

    Revert "Removes InMemoryUnknownAccountService (#705)" (#706)
    This reverts commit 26e4f6281a38916b66eb8d68d0bafb5bc2e17fab.
    
    Adding back InMemoryUnknownAccountService for the users who are not ready to move to containerization.

---
+++ ambry-account/src/main/java/com/github/ambry/account/InMemoryUnknownAccountService.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2017 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package com.github.ambry.account;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+
+
+/**
+ * An implementation of {@link AccountService} that always has a single entry {@link Account#UNKNOWN_ACCOUNT}. Any
+ * queries to this account service will unconditionally return {@link Account#UNKNOWN_ACCOUNT}. This account service
+ * is in memory, and does not talk to any persistent storage service.
+ */
+class InMemoryUnknownAccountService implements AccountService {
+  private static final Collection<Account> accounts =
+      Collections.unmodifiableCollection(Collections.singletonList(Account.UNKNOWN_ACCOUNT));
+  private volatile boolean isOpen = true;
+
+  @Override
+  public Account getAccountById(short accountId) {
+    checkOpen();
+    return Account.UNKNOWN_ACCOUNT;
+  }
+
+  @Override
+  public Account getAccountByName(String accountName) {
+    checkOpen();
+    Objects.requireNonNull(accountName, "accountName cannot be null.");
+    return Account.UNKNOWN_ACCOUNT;
+  }
+
+  @Override
+  public boolean updateAccounts(Collection<Account> accounts) {
+    checkOpen();
+    Objects.requireNonNull(accounts, "accounts cannot be null");
+    return false;
+  }
+
+  @Override
+  public Collection<Account> getAllAccounts() {
+    return accounts;
+  }
+
+  @Override
+  public void close() {
+    isOpen = false;
+  }
+
+  /**
+   * Checks if the service is open.
+   */
+  private void checkOpen() {
+    if (!isOpen) {
+      throw new IllegalStateException("AccountService is closed.");
+    }
+  }
+}
---
+++ ambry-account/src/main/java/com/github/ambry/account/InMemoryUnknownAccountServiceFactory.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package com.github.ambry.account;
+
+import com.codahale.metrics.MetricRegistry;
+import com.github.ambry.commons.Notifier;
+import com.github.ambry.config.VerifiableProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * An {@link AccountServiceFactory} that returns an {@link InMemoryUnknownAccountService}.
+ */
+public class InMemoryUnknownAccountServiceFactory implements AccountServiceFactory {
+  private final Logger logger = LoggerFactory.getLogger(getClass());
+
+  /**
+   * Constructor.
+   * @param verifiableProperties The properties to get a {@link HelixAccountService} instance. Cannot be {@code null}.
+   * @param metricRegistry The {@link MetricRegistry} for metrics tracking. Cannot be {@code null}.
+   * @param notifier The {@link Notifier} used to get a {@link HelixAccountService}.
+   */
+  public InMemoryUnknownAccountServiceFactory(VerifiableProperties verifiableProperties, MetricRegistry metricRegistry,
+      Notifier<String> notifier) {
+  }
+
+  @Override
+  public AccountService getAccountService() {
+    logger.info("Starting an InMemoryUnknownAccountService.");
+    return new InMemoryUnknownAccountService();
+  }
+}
