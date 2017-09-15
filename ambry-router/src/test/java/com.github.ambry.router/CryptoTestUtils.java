1c89f141f70223f51aee2ea0cf94e5663642ad5d
Sivabalan Narayanan <n.siva.b@gmail.com>
2017-09-15 16:34:35 -0700

    Adding KeyManagementService and CryptoService interface and default impl (#710)
    - Adds interfaces for KMS and CryptoService
    - Adds default implementation for the same
    - Unit tests for the default impl

---
+++ ambry-router/src/test/java/com.github.ambry.router/CryptoTestUtils.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package com.github.ambry.router;
+
+import com.github.ambry.utils.TestUtils;
+import java.util.Properties;
+
+
+/**
+ * Utilities used for KMS and CryptoService tests
+ */
+class CryptoTestUtils {
+
+  /**
+   * Constructs and returns a VerifiableProperties instance with the defaults required for instantiating
+   * the {@link SingleKeyManagementService}.
+   * @param keySize kms key size property value
+   * @return the created Properties instance.
+   */
+  static Properties getKMSProperties(String key, int keySize) {
+    Properties properties = new Properties();
+    properties.setProperty("kms.default.container.key", key);
+    properties.setProperty("kms.random.key.size.in.bits", Integer.toString(keySize));
+    properties.setProperty("clustermap.cluster.name", "dev");
+    properties.setProperty("clustermap.datacenter.name", "DC1");
+    properties.setProperty("clustermap.host.name", "localhost");
+    return properties;
+  }
+
+  /**
+   * Generates and returns a random Hex String of the specified size
+   * @param size expected key hex string size
+   * @return the hex string thus generated
+   */
+  static String getRandomKey(int size) {
+    StringBuilder sb = new StringBuilder();
+    while (sb.length() < size) {
+      sb.append(Integer.toHexString(TestUtils.RANDOM.nextInt()));
+    }
+    sb.setLength(size);
+    return sb.toString();
+  }
+}
---
+++ ambry-router/src/test/java/com.github.ambry.router/GCMCryptoServiceTest.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2017 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package com.github.ambry.router;
+
+import com.github.ambry.config.VerifiableProperties;
+import com.github.ambry.utils.TestUtils;
+import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Properties;
+import javax.crypto.spec.SecretKeySpec;
+import org.bouncycastle.util.encoders.Hex;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.github.ambry.router.CryptoTestUtils.*;
+
+
+/**
+ * Tests {@link GCMCryptoService} and {@link GCMCryptoServiceFactory}
+ */
+public class GCMCryptoServiceTest {
+
+  private static final int MAX_DATA_SIZE = 10000;
+  private static final int DEFAULT_KEY_SIZE_IN_CHARS = 64;
+
+  /**
+   * Tests basic encryption and decryption for different sizes of keys and random data in bytes
+   */
+  @Test
+  public void testEncryptDecryptBytes() throws Exception {
+    for (int j = 0; j < 3; j++) {
+      String key = getRandomKey(DEFAULT_KEY_SIZE_IN_CHARS);
+      Properties props = getKMSProperties(key, DEFAULT_KEY_SIZE_IN_CHARS);
+      VerifiableProperties verifiableProperties = new VerifiableProperties((props));
+      SecretKeySpec secretKeySpec = new SecretKeySpec(Hex.decode(key), "AES");
+      CryptoService<SecretKeySpec> cryptoService = new GCMCryptoServiceFactory(verifiableProperties).getCryptoService();
+      for (int i = 0; i < 5; i++) {
+        int size = TestUtils.RANDOM.nextInt(MAX_DATA_SIZE);
+        byte[] randomData = new byte[size];
+        TestUtils.RANDOM.nextBytes(randomData);
+        ByteBuffer toEncrypt = ByteBuffer.wrap(randomData);
+        ByteBuffer encryptedBytes = cryptoService.encrypt(toEncrypt, secretKeySpec);
+        Assert.assertFalse("Encrypted bytes and plain bytes should not match",
+            Arrays.equals(randomData, encryptedBytes.array()));
+        ByteBuffer decryptedBytes = cryptoService.decrypt(encryptedBytes, secretKeySpec);
+        Assert.assertArrayEquals("Decrypted bytes and plain bytes should match", randomData, decryptedBytes.array());
+      }
+    }
+  }
+
+  /**
+   * Tests encryption and decryption of keys with random data
+   */
+  @Test
+  public void testEncryptDecryptKeys() throws Exception {
+    for (int j = 0; j < 5; j++) {
+      String keyToEncrypt = getRandomKey(DEFAULT_KEY_SIZE_IN_CHARS);
+      Properties props = getKMSProperties(keyToEncrypt, DEFAULT_KEY_SIZE_IN_CHARS);
+      VerifiableProperties verifiableProperties = new VerifiableProperties((props));
+      SecretKeySpec secretKeyToEncrypt = new SecretKeySpec(Hex.decode(keyToEncrypt), "AES");
+      String keyToBeEncrypted = getRandomKey(DEFAULT_KEY_SIZE_IN_CHARS);
+      SecretKeySpec secretKeyToBeEncrypted = new SecretKeySpec(Hex.decode(keyToBeEncrypted), "AES");
+      CryptoService<SecretKeySpec> cryptoService = new GCMCryptoServiceFactory(verifiableProperties).getCryptoService();
+      ByteBuffer encryptedBytes = cryptoService.encryptKey(secretKeyToBeEncrypted, secretKeyToEncrypt);
+      Assert.assertFalse("Encrypted key and plain key should not match",
+          Arrays.equals(keyToBeEncrypted.getBytes(), encryptedBytes.array()));
+      SecretKeySpec decryptedKey = cryptoService.decryptKey(encryptedBytes, secretKeyToEncrypt);
+      Assert.assertEquals("Decrypted Key and original key mismatch", secretKeyToBeEncrypted, decryptedKey);
+    }
+  }
+
+  /**
+   * Tests encryption and decryption of keys with diff iv sizes
+   */
+  @Test
+  public void testEncryptDecryptKeysDiffIvSize() throws Exception {
+    int[] ivSizes = {12, 16, 24, 32, 48, 64};
+    for (int ivSize : ivSizes) {
+      String keyToEncrypt = getRandomKey(DEFAULT_KEY_SIZE_IN_CHARS);
+      Properties props = getKMSProperties(keyToEncrypt, DEFAULT_KEY_SIZE_IN_CHARS);
+      props.setProperty("crypto.service.iv.size.in.bytes", Integer.toString(ivSize));
+      VerifiableProperties verifiableProperties = new VerifiableProperties((props));
+      SecretKeySpec secretKeyToEncrypt = new SecretKeySpec(Hex.decode(keyToEncrypt), "AES");
+      String keyToBeEncrypted = getRandomKey(DEFAULT_KEY_SIZE_IN_CHARS);
+      SecretKeySpec secretKeyToBeEncrypted = new SecretKeySpec(Hex.decode(keyToBeEncrypted), "AES");
+      CryptoService<SecretKeySpec> cryptoService = new GCMCryptoServiceFactory(verifiableProperties).getCryptoService();
+      ByteBuffer encryptedBytes = cryptoService.encryptKey(secretKeyToBeEncrypted, secretKeyToEncrypt);
+      Assert.assertFalse("Encrypted key and plain key should not match",
+          Arrays.equals(keyToBeEncrypted.getBytes(), encryptedBytes.array()));
+      SecretKeySpec decryptedKey = cryptoService.decryptKey(encryptedBytes, secretKeyToEncrypt);
+      Assert.assertEquals("Decrypted Key and original key mismatch", secretKeyToBeEncrypted, decryptedKey);
+    }
+  }
+
+  /**
+   * Tests failure scenarios for decryption
+   * @throws InstantiationException
+   */
+  @Test
+  public void testDecryptionFailure() throws InstantiationException {
+    String key = getRandomKey(DEFAULT_KEY_SIZE_IN_CHARS);
+    Properties props = getKMSProperties(key, DEFAULT_KEY_SIZE_IN_CHARS);
+    VerifiableProperties verifiableProperties = new VerifiableProperties((props));
+    SecretKeySpec secretKeySpec = new SecretKeySpec(Hex.decode(key), "AES");
+    CryptoService<SecretKeySpec> cryptoService = new GCMCryptoServiceFactory(verifiableProperties).getCryptoService();
+    int size = TestUtils.RANDOM.nextInt(MAX_DATA_SIZE);
+    byte[] randomData = new byte[size];
+    TestUtils.RANDOM.nextBytes(randomData);
+    ByteBuffer toDecrypt = ByteBuffer.wrap(randomData);
+    try {
+      cryptoService.decrypt(toDecrypt, secretKeySpec);
+      Assert.fail("Decryption should have failed as input data is not encrypted");
+    } catch (GeneralSecurityException e) {
+    }
+
+    try {
+      cryptoService.decryptKey(toDecrypt, secretKeySpec);
+      Assert.fail("Decryption of key should have failed as input data is not encrypted");
+    } catch (GeneralSecurityException e) {
+    }
+  }
+
+  /**
+   * Test {@link GCMCryptoServiceFactory}
+   */
+  @Test
+  public void testGMCryptoServiceFactory() throws Exception {
+    // happy path
+    VerifiableProperties verifiableProperties = new VerifiableProperties((new Properties()));
+    new GCMCryptoServiceFactory(verifiableProperties).getCryptoService();
+
+    // unrecognized mode
+    String key = getRandomKey(DEFAULT_KEY_SIZE_IN_CHARS);
+    Properties props = getKMSProperties(key, DEFAULT_KEY_SIZE_IN_CHARS);
+    props.setProperty("crypto.service.encryption.decryption.mode", "CBC");
+    verifiableProperties = new VerifiableProperties((props));
+    try {
+      CryptoService<SecretKeySpec> cryptoService = new GCMCryptoServiceFactory(verifiableProperties).getCryptoService();
+      Assert.fail("IllegalArgumentException should have thrown for un-recognized mode ");
+    } catch (IllegalArgumentException e) {
+    }
+  }
+}
