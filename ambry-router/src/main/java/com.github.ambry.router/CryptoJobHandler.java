e22e6ce21c1afa712bb20cb05e6aee2be82b17a3
sinaraya <n.siva.b@gmail.com>
2017-09-28 11:45:26 -0700

    Fixing Copyright info - File Header

--- ambry-router/src/main/java/com.github.ambry.router/CryptoJobHandler.java
+++ ambry-router/src/main/java/com.github.ambry.router/CryptoJobHandler.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2017 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
 package com.github.ambry.router;
 
 import com.github.ambry.utils.Utils;
@@ -18,7 +31,7 @@ import org.slf4j.LoggerFactory;
  * {@link #submitJob(CryptoJob)}. On close, all pending jobs will be processed(either successfully or exception is set)
  * and any new jobs submitted after close will be ignored.
  */
-class CryptoJobHandler implements Closeable{
+class CryptoJobHandler implements Closeable {
   private final BlockingQueue<CryptoJob> jobQueue;
   private final List<CryptoWorker> workerThreads;
   private AtomicBoolean enabled;
