415c3650ea0210e5d6e52c9f8dff5a1b883de4b1
David Harju <dharju@linkedin.com>
2018-06-06 16:32:18 -0700

    Public test input streams (#945)
    * Made test MessageFormatInputStream objects public for unit testing in other packages

---
+++ ambry-messageformat/src/test/java/com.github.ambry.messageformat/DeleteMessageFormatV3InputStream.java
@@ -0,0 +1,58 @@
+/**
+ * Copyright 2018 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package com.github.ambry.messageformat;
+
+import com.github.ambry.store.StoreKey;
+import java.nio.ByteBuffer;
+
+
+/**
+ * Represents a message that consists of the delete record in version {@link MessageFormatRecord.Update_Format_V3}
+ * This format is used to delete a blob
+ *
+ *  - - - - - - - - - - - - -
+ * |     Message Header  V2  |
+ *  - - - - - - - - - - - - -
+ * |       blob key          |
+ *  - - - - - - - - - - - - -
+ * |      Update Record      |
+ *  - - - - - - - - - - - - -
+ *
+ */
+public class DeleteMessageFormatV3InputStream extends MessageFormatInputStream {
+  DeleteMessageFormatV3InputStream(StoreKey key, short accountId, short containerId, long deletionTimeMs)
+      throws MessageFormatException {
+    int headerSize = MessageFormatRecord.getHeaderSizeForVersion(MessageFormatRecord.headerVersionToUse);
+    int deleteRecordSize = MessageFormatRecord.Update_Format_V3.getRecordSize(UpdateRecord.Type.DELETE);
+    buffer = ByteBuffer.allocate(headerSize + key.sizeInBytes() + deleteRecordSize);
+    if (MessageFormatRecord.headerVersionToUse == MessageFormatRecord.Message_Header_Version_V1) {
+      MessageFormatRecord.MessageHeader_Format_V1.serializeHeader(buffer, deleteRecordSize,
+          MessageFormatRecord.Message_Header_Invalid_Relative_Offset, headerSize + key.sizeInBytes(),
+          MessageFormatRecord.Message_Header_Invalid_Relative_Offset,
+          MessageFormatRecord.Message_Header_Invalid_Relative_Offset);
+    } else {
+      MessageFormatRecord.MessageHeader_Format_V2.serializeHeader(buffer, deleteRecordSize,
+          MessageFormatRecord.Message_Header_Invalid_Relative_Offset,
+          MessageFormatRecord.Message_Header_Invalid_Relative_Offset, headerSize + key.sizeInBytes(),
+          MessageFormatRecord.Message_Header_Invalid_Relative_Offset,
+          MessageFormatRecord.Message_Header_Invalid_Relative_Offset);
+    }
+    buffer.put(key.toBytes());
+    // set the message as deleted
+    MessageFormatRecord.Update_Format_V3.serialize(buffer,
+        new UpdateRecord(accountId, containerId, deletionTimeMs, new DeleteSubRecord()));
+    messageLength = buffer.capacity();
+    buffer.flip();
+  }
+}
