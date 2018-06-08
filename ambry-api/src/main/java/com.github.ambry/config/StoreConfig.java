fab8ab3944245e6db272356bed4d13462aac444a
Ze Mao <maoze365@googlemail.com>
2018-06-08 12:04:05 -0600

    store.compaction.min.buffer.size minimum value can be 0 (#949)

--- ambry-api/src/main/java/com.github.ambry/config/StoreConfig.java
+++ ambry-api/src/main/java/com.github.ambry/config/StoreConfig.java
@@ -239,7 +239,7 @@ public class StoreConfig {
         verifiableProperties.getIntInRange("store.cleanup.operations.bytes.per.sec", 1 * 1024 * 1024, 1,
             Integer.MAX_VALUE);
     storeCompactionMinBufferSize =
-        verifiableProperties.getIntInRange("store.compaction.min.buffer.size", 10 * 1024 * 1024, 1,
+        verifiableProperties.getIntInRange("store.compaction.min.buffer.size", 10 * 1024 * 1024, 0,
             Integer.MAX_VALUE);
     storeEnableHardDelete = verifiableProperties.getBoolean("store.enable.hard.delete", false);
     storeSegmentSizeInBytes =
