9d5f289e44d64a93fbf5febdee8761eedaa9841c
Priyesh Narayanan <priyesh@gmail.com>
2018-06-26 19:28:01 -0700

    Javadoc for TransformationOutput

--- ambry-api/src/main/java/com.github.ambry/store/TransformationOutput.java
+++ ambry-api/src/main/java/com.github.ambry/store/TransformationOutput.java
@@ -13,19 +13,34 @@
  */
 package com.github.ambry.store;
 
+/**
+ * A class that holds the result of a transformation (by the {@link Transformer}).
+ */
 public class TransformationOutput {
   Exception exception;
   Message msg;
 
+  /**
+   * Instantiate an instance of this class
+   * @param exception the exception encountered with the associated transformation, if any.
+   * @param msg the resulting message after the transformation. Should be null if exception is non-null. May be null
+   *            even if exception is null, which signifies that the transformation resulted in the message being discarded.
+   */
   public TransformationOutput(Exception exception, Message msg) {
     this.exception = exception;
     this.msg = msg;
   }
 
+  /**
+   * @return the {@link Exception}, if any, that the transformation encountered. May be null.
+   */
   public Exception getException() {
     return exception;
   }
 
+  /**
+   * @return the {@link Message}, if any, that the transformation resulted in. May be null.
+   */
   public Message getMsg() {
     return msg;
   }
