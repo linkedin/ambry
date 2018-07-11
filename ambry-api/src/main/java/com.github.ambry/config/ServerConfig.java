9b4e6a83d2935ea55afe4701aa56b6f10d0b1e71
David Harju <dharju@linkedin.com>
2018-07-11 12:32:41 -0700

    Changed server config

--- ambry-api/src/main/java/com.github.ambry/config/ServerConfig.java
+++ ambry-api/src/main/java/com.github.ambry/config/ServerConfig.java
@@ -85,7 +85,7 @@ public class ServerConfig {
         verifiableProperties.getLong("server.quota.stats.aggregate.interval.in.minutes", 60);
     serverEnableStoreDataPrefetch = verifiableProperties.getBoolean("server.enable.store.data.prefetch", false);
     serverStoreKeyConverterFactory = verifiableProperties.getString("server.store.key.converter.factory",
-        "com.github.ambry.server.StoreKeyConverterFactoryImpl");
+        "com.github.ambry.store.StoreKeyConverterFactoryImpl");
     serverMessageTransformer = verifiableProperties.getString("server.message.transformer",
         "com.github.ambry.messageformat.ValidatingTransformer");
   }
