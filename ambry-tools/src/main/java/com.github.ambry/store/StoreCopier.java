c5af17211b47b77439b19a53127fe45eda617917
David Harju <dharju@linkedin.com>
2018-08-01 15:30:23 -0700

    Review comments

--- ambry-tools/src/main/java/com.github.ambry/store/StoreCopier.java
+++ ambry-tools/src/main/java/com.github.ambry/store/StoreCopier.java
@@ -247,7 +247,7 @@ public class StoreCopier implements Closeable {
         }
       }
       token = findInfo.getFindToken();
-      double percentBytesRead = src.getSizeInBytes() > 0L ? token.getBytesRead() * 100.0 / src.getSizeInBytes() : 100.0;
+      double percentBytesRead = src.isEmpty() ? 100.0 : token.getBytesRead() * 100.0 / src.getSizeInBytes() ;
       logger.info("[{}] [{}] {}% copied", Thread.currentThread().getName(), storeId, df.format(percentBytesRead));
     } while (!token.equals(lastToken));
     return new Pair<>(token, sourceHasProblems);
