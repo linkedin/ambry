2cae034a39c74c076d826d5bdacd54ddf358bcc0
Ze Mao <zemao@linkedin.com>
2018-08-13 13:11:13 -0700

    Increase timeout to pass test.

--- ambry-rest/src/test/java/com.github.ambry.rest/NettyMessageProcessorTest.java
+++ ambry-rest/src/test/java/com.github.ambry.rest/NettyMessageProcessorTest.java
@@ -329,7 +329,7 @@ public class NettyMessageProcessorTest {
       }
       channel.writeInbound(LastHttpContent.EMPTY_LAST_CONTENT);
     }
-    if (!notificationSystem.operationCompleted.await(100, TimeUnit.MILLISECONDS)) {
+    if (!notificationSystem.operationCompleted.await(1000, TimeUnit.MILLISECONDS)) {
       fail("Post did not succeed after 100ms. There is an error or timeout needs to increase");
     }
     assertNotNull("Blob id operated on cannot be null", notificationSystem.blobIdOperatedOn);
