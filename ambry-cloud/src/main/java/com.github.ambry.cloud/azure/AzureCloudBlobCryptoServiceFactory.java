b47497984f1c9bd967bff4d719cf379b95436f65
David Harju <dharju@linkedin.com>
2019-03-31 15:54:21 -0700

    Licenses and java doc

--- ambry-cloud/src/main/java/com.github.ambry.cloud/azure/AzureCloudBlobCryptoServiceFactory.java
+++ ambry-cloud/src/main/java/com.github.ambry.cloud/azure/AzureCloudBlobCryptoServiceFactory.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2019 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
 package com.github.ambry.cloud.azure;
 
 import com.codahale.metrics.MetricRegistry;
@@ -11,6 +24,9 @@ import com.github.ambry.utils.Utils;
 import java.security.GeneralSecurityException;
 
 
+/**
+ * Factory for constructing {@link AzureCloudBlobCryptoService} instances.
+ */
 public class AzureCloudBlobCryptoServiceFactory implements CloudBlobCryptoServiceFactory {
 
   private CryptoServiceFactory cryptoServiceFactory;
