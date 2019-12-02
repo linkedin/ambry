7f5f2a2ee8621a9a942365e3b991b5d424576c8f
Casey Getz <cgetz@linkedin.com>
2019-12-02 11:14:15 -0800

    Add copyright

--- ambry-utils/src/main/java/com.github.ambry.utils/CachedHistogram.java
+++ ambry-utils/src/main/java/com.github.ambry.utils/CachedHistogram.java
@@ -1,10 +1,24 @@
+/*
+ * Copyright 2019 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
+
 package com.github.ambry.utils;
 
 import com.codahale.metrics.CachedGauge;
 import com.codahale.metrics.Clock;
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Reservoir;
-import com.codahale.metrics.Snapshot;
 import java.util.concurrent.TimeUnit;
 
 
