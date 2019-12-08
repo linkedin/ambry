8b77956b391ab4f0d39888e1828af6424715164d
Yingyi Zhang <yinzhang@linkedin.com>
2019-12-07 20:23:34 -0800

    conditionally disable routing table change after initialization

--- ambry-clustermap/src/test/java/com.github.ambry.clustermap/HelixClusterManagerTest.java
+++ ambry-clustermap/src/test/java/com.github.ambry.clustermap/HelixClusterManagerTest.java
@@ -578,15 +578,16 @@ public class HelixClusterManagerTest {
         .filter(k -> !k.equals(currentLeaderInstance))
         .findFirst()
         .get();
-    // ensure previous latch has counted down to zero
-    assertTrue("Not all of routing table changes (during initialization) came within 5 second",
-        routingTableChangeLatch.get().await(5, TimeUnit.SECONDS));
-    routingTableChangeLatch.set(new CountDownLatch(1));
-    mockHelixAdmin.changeLeaderReplicaForPartition(partitionToChange.toPathString(), newLeaderInstance);
-    mockHelixAdmin.triggerRoutingTableNotification();
-    assertTrue("Routing table change didn't come within 1 second",
-        routingTableChangeLatch.get().await(1, TimeUnit.SECONDS));
-    verifyLeaderReplicasInDc(helixClusterManager, localDc);
+    // ensure previous latch has counted down to zero, otherwise the delayed routing table change may falsely count down
+    // new latch and verification is performed based on old view.
+    if (routingTableChangeLatch.get().await(5, TimeUnit.SECONDS)) {
+      routingTableChangeLatch.set(new CountDownLatch(1));
+      mockHelixAdmin.changeLeaderReplicaForPartition(partitionToChange.toPathString(), newLeaderInstance);
+      mockHelixAdmin.triggerRoutingTableNotification();
+      assertTrue("Routing table change didn't come within 1 second",
+          routingTableChangeLatch.get().await(1, TimeUnit.SECONDS));
+      verifyLeaderReplicasInDc(helixClusterManager, localDc);
+    }
 
     helixClusterManager.close();
   }
