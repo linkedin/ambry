77f897bab972053ba8eea8d2185a79dc655a9232
Yingyi Zhang <yinzhang@linkedin.com>
2019-12-11 18:21:40 -0800

    try 30 secs

--- ambry-clustermap/src/test/java/com.github.ambry.clustermap/HelixClusterManagerTest.java
+++ ambry-clustermap/src/test/java/com.github.ambry.clustermap/HelixClusterManagerTest.java
@@ -580,11 +580,8 @@ public class HelixClusterManagerTest {
         .get();
     // ensure previous latch has counted down to zero, otherwise the delayed routing table change may falsely count down
     // new latch and verification is performed based on old view. (if condition is not met, skip rest test)
-    long startTimeMs = System.currentTimeMillis();
-    assertTrue("Routing table change didn't come within 10 seconds",
-        routingTableChangeLatch.get().await(1, TimeUnit.MINUTES));
-    long elapsedTimeSec = (System.currentTimeMillis() - startTimeMs) / 1000;
-    System.out.println("Elapsed time for counting down latch: " + elapsedTimeSec);
+    assertTrue("Routing table change didn't come within 30 seconds",
+        routingTableChangeLatch.get().await(30, TimeUnit.SECONDS));
     routingTableChangeLatch.set(new CountDownLatch(1));
     mockHelixAdmin.changeLeaderReplicaForPartition(partitionToChange.toPathString(), newLeaderInstance);
     mockHelixAdmin.triggerRoutingTableNotification();
