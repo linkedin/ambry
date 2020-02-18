4047cd146218d7476f8ace5e5118563b4e5a7f0d
Travis CI <szczepiq@gmail.com>
2020-02-18 13:53:02 -0800

    Updated based on review

--- gradle/git-push.sh
+++ gradle/git-push.sh
@@ -5,7 +5,7 @@
 #for example: mockitoguy:qq43234xc23x23d24d
 
 echo "Running git push without output for security. If it fails make sure that GIT_SECRET env variable is set."
-git push --quiet https://$GIT_SECRET@github.com/mockitoguy/ambry.git --tags > /dev/null 2>&1
+git push --quiet https://$GIT_SECRET@github.com/linkedin/ambry.git --tags > /dev/null 2>&1
 EXIT_CODE=$?
 echo "'git push --quiet' exit code: $EXIT_CODE"
 exit $EXIT_CODE
