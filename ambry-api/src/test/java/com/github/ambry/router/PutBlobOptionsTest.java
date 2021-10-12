7d0ccea7bca3e35bb572f0e86c8d4de4db7db197
Justin Lin <juslin@linkedin.com>
2021-10-12 13:11:41 -0700

    Fix test failures

--- ambry-api/src/test/java/com/github/ambry/router/PutBlobOptionsTest.java
+++ ambry-api/src/test/java/com/github/ambry/router/PutBlobOptionsTest.java
@@ -15,6 +15,10 @@
 
 package com.github.ambry.router;
 
+import com.github.ambry.rest.MockRestRequest;
+import com.github.ambry.rest.RestMethod;
+import com.github.ambry.rest.RestRequest;
+import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
@@ -30,13 +34,19 @@ public class PutBlobOptionsTest {
    * @throws Exception
    */
   @Test
-  public void testOptions() {
+  public void testOptions() throws Exception {
     PutBlobOptions options = new PutBlobOptionsBuilder().chunkUpload(true).build();
     assertTrue("chunkUpload from options not as expected.", options.isChunkUpload());
     assertEquals("maxUploadSize from options not as expected.", Long.MAX_VALUE, options.getMaxUploadSize());
     options = new PutBlobOptionsBuilder().chunkUpload(false).maxUploadSize(3).build();
     assertFalse("chunkUpload from options not as expected.", options.isChunkUpload());
     assertEquals("maxUploadSize from options not as expected.", 3, options.getMaxUploadSize());
+    JSONObject header = new JSONObject();
+    header.put(MockRestRequest.URI_KEY, "/");
+    header.put(MockRestRequest.REST_METHOD_KEY, RestMethod.GET.name());
+    RestRequest restRequest = new MockRestRequest(header, null);
+    options = new PutBlobOptionsBuilder().restRequest(restRequest).build();
+    assertEquals("RestRequest mismatch", restRequest, options.getRestRequest());
   }
 
   /**
@@ -48,7 +58,8 @@ public class PutBlobOptionsTest {
     PutBlobOptions b = new PutBlobOptionsBuilder().chunkUpload(true).maxUploadSize(3).build();
     assertEquals("PutBlobOptions should be equal", a, b);
     assertEquals("PutBlobOptions hashcodes should be equal", a.hashCode(), b.hashCode());
-    assertEquals("toString output not as expected", "PutBlobOptions{chunkUpload=true, maxUploadSize=3}", a.toString());
+    assertEquals("toString output not as expected",
+        "PutBlobOptions{chunkUpload=true, maxUploadSize=3, restRequest=null}", a.toString());
     b = new PutBlobOptionsBuilder().chunkUpload(false).maxUploadSize(3).build();
     assertThat("PutBlobOptions should not be equal.", a, not(b));
   }
