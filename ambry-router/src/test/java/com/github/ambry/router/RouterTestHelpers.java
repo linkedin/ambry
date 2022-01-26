66397511f7eb76a72107ca48ba38cd6018a61aa4
Ankur Agrawal <ankuagrawal@linkedin.com>
2022-01-26 06:19:02 -0800

    Surround NonBlockingRouterTests with try-catch block to make sure cleanup happens for each test, and one test failure doesn't cause subsequent tests to fail. Also change timeout to wait for test operations to complete to 15 secs from 10 secs. (#2033)
    Surround NonBlockingRouterTests with try-catch block to make sure cleanup happens for each test, 
    and one test failure doesn't cause subsequent tests to fail. Also change timeout to wait for test 
    operations to complete to 15 secs from 10 secs.
    
    NOTE: The only change in this file is to surround the tests that attempt to close router at the end, 
    with try-catch block. There are no logic changes in this file. Note that it might be possible to unify
    the cleanup logic by refactoring the code a little. But this change keeps that out of its scope.

--- ambry-router/src/test/java/com/github/ambry/router/RouterTestHelpers.java
+++ ambry-router/src/test/java/com/github/ambry/router/RouterTestHelpers.java
@@ -51,7 +51,7 @@ import static org.junit.Assert.*;
  */
 class RouterTestHelpers {
   private static final int AWAIT_TIMEOUT_SECONDS = 10;
-  static final int AWAIT_TIMEOUT_MS = 10000;
+  static final int AWAIT_TIMEOUT_MS = 15000;
   private static final short BLOB_ID_VERSION = CommonTestUtils.getCurrentBlobIdVersion();
 
   /**
