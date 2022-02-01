968622e4995bcfca1076fd05655552e7749167c8
Travis CI <travis@travis-ci.org>
2022-02-01 15:36:27 -0800

    Add utilities in InMemoryAccountService to create random account with specified QuotaResourceType.

--- ambry-test-utils/src/main/java/com/github/ambry/account/InMemAccountService.java
+++ ambry-test-utils/src/main/java/com/github/ambry/account/InMemAccountService.java
@@ -166,7 +166,19 @@ public class InMemAccountService implements AccountService {
    * @return the {@link Account} that was created and added.
    */
   public synchronized Account createAndAddRandomAccount() {
-    Account account = generateRandomAccount();
+    return createAndAddRandomAccount(
+        QuotaResourceType.values()[Utils.getRandomShort(TestUtils.RANDOM) % QuotaResourceType.values().length]);
+  }
+
+  /**
+   * Creates and adds an {@link Account} to this {@link AccountService}. The account will contain one container
+   * with {@link Container#DEFAULT_PUBLIC_CONTAINER_ID}, one with {@link Container#DEFAULT_PRIVATE_CONTAINER_ID} and
+   * one other random {@link Container}.
+   * @param quotaResourceType {@link QuotaResourceType} for the account.
+   * @return the {@link Account} that was created and added.
+   */
+  public synchronized Account createAndAddRandomAccount(QuotaResourceType quotaResourceType) {
+    Account account = generateRandomAccount(quotaResourceType);
     try {
       updateAccounts(Collections.singletonList(account));
     } catch (AccountServiceException ase) {
@@ -182,6 +194,18 @@ public class InMemAccountService implements AccountService {
    * @return the {@link Account} that was created.
    */
   public synchronized Account generateRandomAccount() {
+    return generateRandomAccount(
+        QuotaResourceType.values()[Utils.getRandomShort(TestUtils.RANDOM) % QuotaResourceType.values().length]);
+  }
+
+  /**
+   * Generates an {@link Account} but does not add it to this {@link AccountService}. The account will contain one
+   * container with {@link Container#DEFAULT_PUBLIC_CONTAINER_ID}, one with
+   * {@link Container#DEFAULT_PRIVATE_CONTAINER_ID} and one other random {@link Container}.
+   * @param quotaResourceType {@link QuotaResourceType} for the account.
+   * @return the {@link Account} that was created.
+   */
+  public synchronized Account generateRandomAccount(QuotaResourceType quotaResourceType) {
     short refAccountId;
     String refAccountName;
     do {
@@ -198,8 +222,6 @@ public class InMemAccountService implements AccountService {
         new ContainerBuilder(Container.DEFAULT_PRIVATE_CONTAINER).setParentAccountId(refAccountId)
             .setNamedBlobMode(Container.NamedBlobMode.OPTIONAL)
             .build();
-    QuotaResourceType quotaResourceType =
-        QuotaResourceType.values()[Utils.getRandomShort(TestUtils.RANDOM) % QuotaResourceType.values().length];
     return new AccountBuilder(refAccountId, refAccountName, refAccountStatus, quotaResourceType).addOrUpdateContainer(
         publicContainer).addOrUpdateContainer(privateContainer).addOrUpdateContainer(randomContainer).build();
   }
