4f7a11622a1cc71153a9c981d8e0bca782ec0eb9
Arun Sai <asai@asai-mn2.linkedin.biz>
2022-05-10 22:07:02 -0700

    Refactor cloud blob store to be asynchronous
    This change adds asynchronous methods for PUT/GET/DELETE/UNDELETE/UPDATE_TTL operations to CloudBlobStore.

--- ambry-cloud/src/main/java/com/github/ambry/cloud/CloudMessageFormatWriteSet.java
+++ ambry-cloud/src/main/java/com/github/ambry/cloud/CloudMessageFormatWriteSet.java
@@ -16,12 +16,14 @@ package com.github.ambry.cloud;
 import com.github.ambry.cloud.CloudBlobStore.CloudWriteChannel;
 import com.github.ambry.messageformat.MessageFormatWriteSet;
 import com.github.ambry.store.MessageInfo;
-import com.github.ambry.store.StoreException;
-import com.github.ambry.utils.Utils;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 /**
@@ -37,9 +39,17 @@ public class CloudMessageFormatWriteSet extends MessageFormatWriteSet {
    * Write the messages in this set to the given write channel asynchronously
    * @param writeChannel The write interface to write the messages to
    * @return a {@link CompletableFuture} that will eventually contain the size in bytes that was written to the
-   *         write interface or the {@link StoreException} if an error occurred
+   *         write interface or the {@link CloudStorageException} if an error occurred
    */
-  public CompletableFuture<Long> writeAsyncTo(CloudWriteChannel writeChannel) {
-    return Utils.completedExceptionally(new UnsupportedOperationException("Async write operation not supported"));
+  CompletableFuture<Long> writeAsyncTo(CloudWriteChannel writeChannel) {
+    ReadableByteChannel readableByteChannel = Channels.newChannel(streamToWrite);
+    AtomicLong sizeWritten = new AtomicLong();
+    List<CompletableFuture<Void>> operationFutures = new ArrayList<>();
+    for (MessageInfo info : streamInfo) {
+      operationFutures.add(writeChannel.appendAsyncFrom(readableByteChannel, info.getSize())
+          .thenRun(() -> sizeWritten.addAndGet(info.getSize())));
+    }
+    return CompletableFuture.allOf(operationFutures.toArray(new CompletableFuture<?>[0]))
+        .thenApply(unused -> sizeWritten.get());
   }
 }
