c0dc24f77527890399f1ed1d45ef7cc5906accfe
Sanketh Nalli <snalli@linkedin.com>
2022-06-14 14:47:21 -0700

    add license
    add license

--- ambry-commons/src/main/java/com/github/ambry/commons/MetadataChunk.java
+++ ambry-commons/src/main/java/com/github/ambry/commons/MetadataChunk.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2022 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 package com.github.ambry.commons;
 
 import com.github.ambry.messageformat.BlobInfo;
@@ -5,25 +19,17 @@ import com.github.ambry.messageformat.CompositeBlobInfo;
 
 public class MetadataChunk {
 
-  private BlobInfo blobInfo;
-  private CompositeBlobInfo compositeBlobInfo;
-
-  public MetadataChunk(CompositeBlobInfo compositeBlobInfo) {
-    this.blobInfo = null;
-    this.compositeBlobInfo = compositeBlobInfo;
-  }
+  private final BlobInfo blobInfo;
+  private final CompositeBlobInfo compositeBlobInfo;
 
   public MetadataChunk(BlobInfo blobInfo, CompositeBlobInfo compositeBlobInfo) {
     this.blobInfo = blobInfo;
     this.compositeBlobInfo = compositeBlobInfo;
   }
-  
-  public BlobInfo getBlobInfo() {
-    return blobInfo;
-  }
 
   public CompositeBlobInfo getCompositeBlobInfo() {
     return compositeBlobInfo;
   }
+  public BlobInfo getBlobInfo() { return blobInfo; }
 
 }
