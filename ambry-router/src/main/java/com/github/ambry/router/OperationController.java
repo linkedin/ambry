7005333432d80476d40eac12c1bb3870d3dac7f2
Thanh Tung Tran <thatran@thatran-mn1.linkedin.biz>
2022-08-24 16:32:57 -0400

    parsing partial header from router to and handling partial get flow

--- ambry-router/src/main/java/com/github/ambry/router/OperationController.java
+++ ambry-router/src/main/java/com/github/ambry/router/OperationController.java
@@ -20,6 +20,7 @@ import com.github.ambry.commons.Callback;
 import com.github.ambry.commons.ResponseHandler;
 import com.github.ambry.config.RouterConfig;
 import com.github.ambry.messageformat.BlobProperties;
+import com.github.ambry.named.PartiallyReadableBlobDb;
 import com.github.ambry.network.NetworkClient;
 import com.github.ambry.network.NetworkClientFactory;
 import com.github.ambry.network.RequestInfo;
@@ -163,8 +164,9 @@ public class OperationController implements Runnable {
    * @param callback The callback which will be invoked on the completion of the request.
    */
   protected void getBlob(String blobIdStr, GetBlobOptionsInternal options,
-      final Callback<GetBlobResultInternal> callback, QuotaChargeCallback quotaChargeCallback) throws RouterException {
-    getManager.submitGetBlobOperation(blobIdStr, options, callback, quotaChargeCallback);
+      final Callback<GetBlobResultInternal> callback, QuotaChargeCallback quotaChargeCallback,
+      PartiallyReadableBlobDb partiallyReadableBlobDb) throws RouterException {
+    getManager.submitGetBlobOperation(blobIdStr, options, callback, quotaChargeCallback, partiallyReadableBlobDb);
     routerCallback.onPollReady();
   }
 
@@ -325,7 +327,7 @@ public class OperationController implements Runnable {
           .build();
       GetBlobOptionsInternal optionsInternal = new GetBlobOptionsInternal(options, true, helper.getMetrics());
       try {
-        getBlob(blobIdStr, optionsInternal, internalCallback, quotaChargeCallback);
+        getBlob(blobIdStr, optionsInternal, internalCallback, quotaChargeCallback, null);
       } catch (RouterException e) {
         helper.getCompleteOperationAtException().accept(e);
       }
