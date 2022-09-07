80a8c6f4bbbcec079c38cc1f16587f161ceac922
Mike Yang <mikeyang@linkedin.com>
2022-09-07 19:43:56 -0400

    Add log for debugging named blob get and list issues (#2207)
    * Add log for named blob get and list issues
    
    * Update log level

--- ambry-named-mysql/src/main/java/com/github/ambry/named/MySqlNamedBlobDb.java
+++ ambry-named-mysql/src/main/java/com/github/ambry/named/MySqlNamedBlobDb.java
@@ -268,6 +268,8 @@ class MySqlNamedBlobDb implements NamedBlobDb {
         } catch (Exception e) {
           if (e instanceof RestServiceException &&
               ((RestServiceException) e).getErrorCode() == RestServiceErrorCode.NotFound) {
+            logger.trace("NAMED GET: Data NotFound in new table. accountId='{}', containerId='{}', blobName='{}'.",
+                accountId, containerId, blobName);
             metricsRecoder.namedDataNotFoundGetCount.inc();
           } else {
             logger.error("NAMED GET: Data Error: old table succeed while new table failed: ", e);
@@ -294,6 +296,9 @@ class MySqlNamedBlobDb implements NamedBlobDb {
           logger.error("NAMED LIST: Data Error: old table succeed while new table failed: ", e);
         }
         if (!Objects.equals(recordPage.getEntries(), recordPageV2.getEntries())) {
+          logger.trace(
+              "NAMED LIST: Data Inconsistent. accountId='{}', containerId='{}', blobNamePrefix='{}', pageToken='{}'.",
+              accountId, containerId, blobNamePrefix, pageToken);
           metricsRecoder.namedDataInconsistentListCount.inc();
         }
       }
