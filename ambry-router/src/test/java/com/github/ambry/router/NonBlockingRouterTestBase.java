248c65c59ba9815faa172a473c54b890b4a71e26
Jing Qian <jiqian@jiqian-mn1.linkedin.biz>
2022-11-02 09:23:37 -0700

    Only make NonBlockingRouterTest uses QuotaAwareOperationController. NonBlockingQuotaTest still uses NonBlockingOperationController.

--- ambry-router/src/test/java/com/github/ambry/router/NonBlockingRouterTestBase.java
+++ ambry-router/src/test/java/com/github/ambry/router/NonBlockingRouterTestBase.java
@@ -195,7 +195,6 @@ public class NonBlockingRouterTestBase {
     properties.setProperty("router.not.found.cache.ttl.in.ms", String.valueOf(NOT_FOUND_CACHE_TTL_MS));
     properties.setProperty("router.get.eligible.replicas.by.state.enabled", "true");
     properties.setProperty("router.repair.with.replicate.blob.enabled", "true");
-    properties.setProperty("router.operation.controller", "com.github.ambry.router.QuotaAwareOperationController");
 
     return properties;
   }
