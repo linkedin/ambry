d84f6b09a1e4ce194b8f7fa7ea44aec22fb3373d
Jing Qian <jiqian@jiqian-mn1.linkedin.biz>
2023-02-13 17:31:21 -0800

    Add separated metrics for ReplicateBlob on Delete and TtlUpdate.

--- ambry-router/src/main/java/com/github/ambry/router/NonBlockingRouter.java
+++ ambry-router/src/main/java/com/github/ambry/router/NonBlockingRouter.java
@@ -356,6 +356,11 @@ class NonBlockingRouter implements Router {
     currentOperationsCount.incrementAndGet();
     routerMetrics.replicateBlobOperationRate.mark();
     routerMetrics.operationQueuingRate.mark();
+    if (serviceId.equals("DeleteOperation")) {
+      routerMetrics.replicateBlobOperationOnDeleteRate.mark();
+    } else if (serviceId.equals("TtlUpdateOperation")){
+      routerMetrics.replicateBlobOperationOnTtlUpdateRate.mark();
+    }
 
     FutureResult<Void> futureResult = new FutureResult<>();
     if (isOpen.get()) {
