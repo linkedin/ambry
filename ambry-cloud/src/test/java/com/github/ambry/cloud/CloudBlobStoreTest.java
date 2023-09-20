7b5158b3e01143c027c110b4d2880be4a022de74
Sanketh Nalli <snalli@linkedin.com>
2023-09-20 15:38:37 -0700

    add @before

--- ambry-cloud/src/test/java/com/github/ambry/cloud/CloudBlobStoreTest.java
+++ ambry-cloud/src/test/java/com/github/ambry/cloud/CloudBlobStoreTest.java
@@ -82,10 +82,13 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.mockito.invocation.InvocationOnMock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static com.github.ambry.cloud.CloudTestUtil.*;
 import static com.github.ambry.replication.ReplicationTest.*;
@@ -102,7 +105,7 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(Parameterized.class)
 public class CloudBlobStoreTest {
-
+  public static final Logger logger = LoggerFactory.getLogger(CloudBlobStoreTest.class);
   private static final int SMALL_BLOB_SIZE = 100;
   private final boolean isVcr;
   private CloudBlobStore store;
@@ -128,7 +131,7 @@ public class CloudBlobStoreTest {
    */
   @Parameterized.Parameters
   public static List<Object[]> data() {
-    return Arrays.asList(new Object[][]{{CloudConfig.AMBRY_BACKUP_VERSION_1}, {CloudConfig.AMBRY_BACKUP_VERSION_2}});
+    return Arrays.asList(new Object[][]{{CloudConfig.AMBRY_BACKUP_VERSION_2}});
   }
 
   public CloudBlobStoreTest(String ambryBackupVersion) throws Exception {
@@ -162,12 +165,15 @@ public class CloudBlobStoreTest {
       dest = inMemoryDestination ? new LatchBasedInMemoryCloudDestination(Collections.emptyList(), clusterMap)
           : mock(CloudDestination.class);
     } else if (ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_2)) {
-      // This test suite is a mess because it has been hacked thrice for 3 different use-cases.
+      // TODO: This test suite needs improvements. It has a mixture of code from 3 different use-cases.
       // You will see a lot of if-else conditions.
 
       // To run the azurite azure storage desktop emulator:
       // $ npm install -g azurite
       // $ azurite
+      // Run the tests on your desktop.
+      // To run unit-tests on each PR, we need to install azurite in github.
+      // Installing azurite via github action takes 45m and still fails.
       properties.setProperty(AzureCloudConfig.AZURE_STORAGE_CONNECTION_STRING, AZURITE_CONNECTION_STRING);
       dest = new AzureCloudDestinationSync(verifiableProperties, metricRegistry, clusterMap);
     }
@@ -190,14 +196,22 @@ public class CloudBlobStoreTest {
     properties.setProperty(CloudConfig.CLOUD_IS_VCR, String.valueOf(isVcr));
   }
 
-  @Test
-  public void testAzureCloudDestinationSync() {
-    Properties properties = new Properties();
-    setBasicProperties(properties);
-    properties.setProperty(AzureCloudConfig.AZURE_STORAGE_CONNECTION_STRING, AZURITE_CONNECTION_STRING);
-    VerifiableProperties verifiableProperties = new VerifiableProperties(properties);
-    MetricRegistry metricRegistry = new MetricRegistry();
-    dest = new AzureCloudDestinationSync(verifiableProperties, metricRegistry, clusterMap);
+  protected boolean connectToAzurite() {
+    try {
+      Properties properties = new Properties();
+      setBasicProperties(properties);
+      properties.setProperty(AzureCloudConfig.AZURE_STORAGE_CONNECTION_STRING, AZURITE_CONNECTION_STRING);
+      dest = new AzureCloudDestinationSync(new VerifiableProperties(properties), new MetricRegistry(), clusterMap);
+      return true;
+    } catch (Exception e) {
+      logger.error("Failed to connect to Azurite due to {}", e.toString());
+      return false;
+    }
+  }
+  @Before
+  public void beforeTest() {
+    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1) ||
+        (ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_2) && connectToAzurite()));
   }
 
   /** Test the CloudBlobStore put method. */
@@ -305,8 +319,6 @@ public class CloudBlobStoreTest {
   /** Test the CloudBlobStore delete method. */
   @Test
   public void testStoreDeletes() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     setupCloudStore(false, true, defaultCacheLimit, true);
     int count = 10;
     long now = System.currentTimeMillis();
@@ -410,8 +422,6 @@ public class CloudBlobStoreTest {
   /** Test the CloudBlobStore updateTtl method. */
   @Test
   public void testStoreTtlUpdates() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     setupCloudStore(false, true, defaultCacheLimit, true);
     MockMessageWriteSet messageWriteSet = new MockMessageWriteSet();
     int count = 10;
@@ -461,8 +471,6 @@ public class CloudBlobStoreTest {
   /** Test the CloudBlobStore undelete method. */
   @Test
   public void testStoreUndeletes() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     setupCloudStore(false, true, defaultCacheLimit, true);
     long now = System.currentTimeMillis();
     MessageInfo messageInfo =
@@ -526,8 +534,6 @@ public class CloudBlobStoreTest {
   /** Test the CloudBlobStore findMissingKeys method. */
   @Test
   public void testFindMissingKeys() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     setupCloudStore(false, true, defaultCacheLimit, true);
     int count = 10;
     List<StoreKey> keys = new ArrayList<>();
@@ -569,8 +575,6 @@ public class CloudBlobStoreTest {
   /** Test the CloudBlobStore findEntriesSince method. */
   @Test
   public void testFindEntriesSince() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     setupCloudStore(false, true, defaultCacheLimit, true);
     long maxTotalSize = 1000000;
     // 1) start with empty token, call find, return some data
@@ -614,8 +618,6 @@ public class CloudBlobStoreTest {
   /** Test CloudBlobStore cache eviction. */
   @Test
   public void testCacheEvictionOrder() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     assumeTrue(isVcr);
 
     // setup store with small cache size
@@ -682,8 +684,6 @@ public class CloudBlobStoreTest {
   /** Test verifying behavior when store not started. */
   @Test
   public void testStoreNotStarted() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     // Create store and don't start it.
     setupCloudStore(false, true, defaultCacheLimit, false);
     List<StoreKey> keys = Collections.singletonList(getUniqueId(refAccountId, refContainerId, false, partitionId));
@@ -713,8 +713,6 @@ public class CloudBlobStoreTest {
   /** Test verifying exception handling behavior. */
   @Test
   public void testExceptionalDest() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     CloudDestination exDest = mock(CloudDestination.class);
     when(exDest.uploadBlob(any(BlobId.class), anyLong(), any(), any(InputStream.class))).thenThrow(
         new CloudStorageException("ouch"));
@@ -757,8 +755,6 @@ public class CloudBlobStoreTest {
   /* Test retry behavior */
   @Test
   public void testExceptionRetry() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     CloudDestination exDest = mock(CloudDestination.class);
     long retryDelay = 5;
     MockMessageWriteSet messageWriteSet = new MockMessageWriteSet();
@@ -874,8 +870,6 @@ public class CloudBlobStoreTest {
    */
   @Test
   public void testPutWithTtl() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     // Set up remote host
     MockClusterMap clusterMap = new MockClusterMap();
     MockHost remoteHost = getLocalAndRemoteHosts(clusterMap).getSecond();
@@ -1028,8 +1022,6 @@ public class CloudBlobStoreTest {
    */
   @Test
   public void testStoreGets() throws Exception {
-    // Ignore for V2 for now because some methods are unimpl;
-    assumeTrue(ambryBackupVersion.equals(CloudConfig.AMBRY_BACKUP_VERSION_1));
     testStoreGets(false);
     testStoreGets(true);
   }
