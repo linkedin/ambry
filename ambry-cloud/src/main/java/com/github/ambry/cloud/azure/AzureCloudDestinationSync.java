ec10b0a74ee949c9084f3d78319c42f767ea0f55
Sanketh Nalli <snalli@linkedin.com>
2023-10-13 16:41:22 -0700

    add back metric

--- ambry-cloud/src/main/java/com/github/ambry/cloud/azure/AzureCloudDestinationSync.java
+++ ambry-cloud/src/main/java/com/github/ambry/cloud/azure/AzureCloudDestinationSync.java
@@ -273,7 +273,7 @@ public class AzureCloudDestinationSync implements CloudDestination {
       if (e instanceof BlobStorageException
           && ((BlobStorageException) e).getErrorCode() == BlobErrorCode.BLOB_ALREADY_EXISTS) {
         // Since VCR replicates from all replicas, a blob can be uploaded by at least two threads concurrently.
-        // azureMetrics.blobUploadConflictCount.inc(); Not a very useful metric
+        azureMetrics.blobUploadConflictCount.inc();
         logger.trace("Failed to upload blob {} to Azure blob storage because it already exists", blobLayout);
         // We should rarely be here because we get here from replication logic which checks if a blob exists or not before uploading it.
         // However, if we end up here, return true to allow replication to proceed instead of halting it. Else the replication token will not advance.
