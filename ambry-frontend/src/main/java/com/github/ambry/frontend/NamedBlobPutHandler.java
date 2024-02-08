6a64918649ae95ddc48578623dd443b00a01e949
Ankur Agrawal <ankuagrawal@linkedin.com>
2024-02-08 12:50:03 -0800

    Add metrics to get the time taken for deserializing and validating signed ids for named blob stitch puts (#2702)
    Co-authored-by: github-actions <github-actions@github.com>

--- ambry-frontend/src/main/java/com/github/ambry/frontend/NamedBlobPutHandler.java
+++ ambry-frontend/src/main/java/com/github/ambry/frontend/NamedBlobPutHandler.java
@@ -455,6 +455,7 @@ public class NamedBlobPutHandler {
      */
     List<ChunkInfo> getChunksToStitch(BlobProperties stitchedBlobProperties, JSONObject stitchRequestJson)
         throws RestServiceException {
+      long getChunksToStitchStartTime = System.currentTimeMillis();
       String reservedMetadataBlobId = null;
       List<String> signedChunkIds = StitchRequestSerDe.fromJson(stitchRequestJson);
       if (signedChunkIds.isEmpty()) {
@@ -493,6 +494,11 @@ public class NamedBlobPutHandler {
       }
       //the actual blob size for stitched blob is the sum of all the chunk sizes
       restResponseChannel.setHeader(RestUtils.Headers.BLOB_SIZE, totalStitchedBlobSize);
+      long getChunksToStitchTimeInMs = System.currentTimeMillis() - getChunksToStitchStartTime;
+      frontendMetrics.namedBlobPutGetChunksToStitchTimeInMs.update(getChunksToStitchTimeInMs);
+      if (getChunksToStitchTimeInMs > 10000) {
+        LOGGER.warn("getChunksToStitch took {} ms for {} chunks", getChunksToStitchTimeInMs, signedChunkIds.size());
+      }
       return chunksToStitch;
     }
 
