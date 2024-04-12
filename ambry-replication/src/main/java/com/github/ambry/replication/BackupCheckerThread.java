1ea8c70741fef986cf2195db8af133458f856622
Sanketh Nalli <snalli@linkedin.com>
2024-04-11 23:02:27 -0700

    rm token bin

--- ambry-replication/src/main/java/com/github/ambry/replication/BackupCheckerThread.java
+++ ambry-replication/src/main/java/com/github/ambry/replication/BackupCheckerThread.java
@@ -261,7 +261,6 @@ public class BackupCheckerThread extends ReplicaThread {
     }
     // Maintain alphabetical order of fields for string match
     json.put("replica_token", rinfo.getToken().toString());
-    json.put("replica_token_binary", new String(rinfo.getToken().toBytes(), StandardCharsets.UTF_8));
     json.put("replicated_until", rinfo.getReplicatedUntilUTC());
     // Pretty print with indent for easy viewing
     // This will help us know when to stop DR process for sealed partitions
