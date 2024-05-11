7d1b05534ccbd67170efc70067a3b4027931db3f
Sanketh Nalli <snalli@linkedin.com>
2024-05-10 17:06:04 -0700

    [DR] Set default recovery token-factory (#2774)
    [DR] Set default recovery token-factory

--- ambry-api/src/main/java/com/github/ambry/config/ReplicationConfig.java
+++ ambry-api/src/main/java/com/github/ambry/config/ReplicationConfig.java
@@ -45,8 +45,9 @@ public class ReplicationConfig {
   /**
    * The factory class the replication uses to create cloud token
    */
+  public static final String DEFAULT_RECOVERY_TOKEN_FACTORY = "com.github.ambry.cloud.RecoveryTokenFactory";
   @Config(REPLICATION_CLOUD_TOKEN_FACTORY)
-  @Default("com.github.ambry.cloud.azure.CosmosUpdateTimeFindTokenFactory")
+  @Default(DEFAULT_RECOVERY_TOKEN_FACTORY)
   public final String replicationCloudTokenFactory;
 
   /**
@@ -325,7 +326,7 @@ public class ReplicationConfig {
     replicationStoreTokenFactory =
         verifiableProperties.getString("replication.token.factory", "com.github.ambry.store.StoreFindTokenFactory");
     replicationCloudTokenFactory = verifiableProperties.getString(REPLICATION_CLOUD_TOKEN_FACTORY,
-        "com.github.ambry.cloud.azure.CosmosChangeFeedFindTokenFactory");
+        DEFAULT_RECOVERY_TOKEN_FACTORY);
     replicationNumOfIntraDCReplicaThreads =
         verifiableProperties.getInt("replication.no.of.intra.dc.replica.threads", 1);
     replicationNumOfInterDCReplicaThreads =
