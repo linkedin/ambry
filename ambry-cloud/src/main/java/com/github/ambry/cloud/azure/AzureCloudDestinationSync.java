1b4e18c31d72ad4e4701719d678e0dec53847bb5
Sanketh Nalli <snalli@linkedin.com>
2024-07-19 19:01:18 -0700

    comment

--- ambry-cloud/src/main/java/com/github/ambry/cloud/azure/AzureCloudDestinationSync.java
+++ ambry-cloud/src/main/java/com/github/ambry/cloud/azure/AzureCloudDestinationSync.java
@@ -679,7 +679,7 @@ public class AzureCloudDestinationSync implements CloudDestination {
           "CUSTOMER_DELETE_REQUEST");
     }
 
-
+    // AzureBlobDeletePolicy.EVENTUAL
     Map<String, Object> newMetadata = new HashMap<>();
     newMetadata.put(CloudBlobMetadata.FIELD_DELETION_TIME, String.valueOf(deletionTime));
     newMetadata.put(CloudBlobMetadata.FIELD_LIFE_VERSION, lifeVersion);
