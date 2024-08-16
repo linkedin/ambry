d3c2c0ec0d0489bf3d608d9f6977a38b23643b13
Sanketh Nalli <snalli@linkedin.com>
2024-08-15 22:01:58 -0700

    fixed + random delay

--- ambry-cloud/src/main/java/com/github/ambry/cloud/VcrReplicaThread.java
+++ ambry-cloud/src/main/java/com/github/ambry/cloud/VcrReplicaThread.java
@@ -104,7 +104,7 @@ public class VcrReplicaThread extends ReplicaThread {
   @Override
   public void run() {
     try {
-      int delay = new Random().nextInt(vcrNodeConfig.backupStartupDelaySecs) + 10;
+      int delay = vcrNodeConfig.backupStartupDelaySecs + new Random().nextInt(180); // fixed + small random delay
       logger.info("Starting replica thread {} in {} seconds", Thread.currentThread().getName(), delay);
       Thread.sleep(TimeUnit.SECONDS.toMillis(delay));
     } catch (InterruptedException e) {
