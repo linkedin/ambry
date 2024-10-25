25a09cf2bfb715a91777b3ad4269058f3aff9e7b
Arun Sai Bhima <asai@linkedin.com>
2024-10-24 21:58:54 -0700

    Moved PipedAsyncWritableChannel to closed source and re-use restServerRouterFactory (#2929)
    This change:
    
    1. Moves PipedAsyncWritableChannel to AmbryLI
    2. Reuses restServerRouterFactory instead of boolean to switch between routers

--- ambry-api/src/main/java/com/github/ambry/config/RestServerConfig.java
+++ ambry-api/src/main/java/com/github/ambry/config/RestServerConfig.java
@@ -20,9 +20,6 @@ public class RestServerConfig {
   public static final String CONFIG_PREFIX = "rest.server.";
   public static final String ENABLE_ADDED_CHANNEL_HANDLERS = CONFIG_PREFIX + "enable.added.channel.handlers";
   public static final String ENABLE_STRUCTURED_LOGGING = CONFIG_PREFIX + "enable.structured.logging";
-  public static final String ENABLE_COMPOSITE_ROUTER = CONFIG_PREFIX + "enable.composite.router";
-  private static final String PIPED_ASYNC_CHANNEL_SECONDARY_TIMEOUT_MS =
-      CONFIG_PREFIX + "piped.async.channel.secondary.timeout.ms";
 
   /**
    * The RestRequestServiceFactory that needs to be used by the RestServer
@@ -77,14 +74,6 @@ public class RestServerConfig {
   public final String restServerRouterFactory;
 
   /**
-   * The CompositeRouterFactory that needs to be used by the RestServer
-   * for bootstrapping the CompositeRouter.
-   */
-  @Config("rest.server.composite.router.factory")
-  @Default("")
-  public final String restServerCompositeRouterFactory;
-
-  /**
    * Request Headers that needs to be logged as part of public access log entries
    */
   @Config("rest.server.public.access.log.request.headers")
@@ -119,20 +108,6 @@ public class RestServerConfig {
   @Default("false")
   public final boolean restServerEnableAddedChannelHandlers;
 
-  /**
-   * Timeout when writing to secondary reader via PipedAsyncWritableChannel.
-   */
-  @Config(PIPED_ASYNC_CHANNEL_SECONDARY_TIMEOUT_MS)
-  @Default("100")
-  public final int restServerPipedAsyncChannelSecondaryTimeoutMs;
-
-  /**
-   * Set true to enable composite router in RestServer.
-   */
-  @Config(ENABLE_COMPOSITE_ROUTER)
-  @Default("false")
-  public final boolean restServerEnableCompositeRouter;
-
   public RestServerConfig(VerifiableProperties verifiableProperties) {
     restServerRestRequestServiceFactory = verifiableProperties.getString("rest.server.rest.request.service.factory");
     restServerNioServerFactory =
@@ -146,7 +121,6 @@ public class RestServerConfig {
         verifiableProperties.getIntInRange("rest.server.response.handler.scaling.unit.count", 5, 0, Integer.MAX_VALUE);
     restServerAccountServiceFactory = verifiableProperties.getString("rest.server.account.service.factory",
         "com.github.ambry.account.InMemoryUnknownAccountServiceFactory");
-    restServerCompositeRouterFactory = verifiableProperties.getString("rest.server.composite.router.factory", "");
     restServerRouterFactory = verifiableProperties.getString("rest.server.router.factory",
         "com.github.ambry.router.NonBlockingRouterFactory");
     restServerPublicAccessLogRequestHeaders =
@@ -157,8 +131,5 @@ public class RestServerConfig {
     restServerEnableStructuredLogging = verifiableProperties.getBoolean(ENABLE_STRUCTURED_LOGGING, false);
     restServerHealthCheckUri = verifiableProperties.getString("rest.server.health.check.uri", "/healthCheck");
     restServerEnableAddedChannelHandlers = verifiableProperties.getBoolean(ENABLE_ADDED_CHANNEL_HANDLERS, false);
-    restServerEnableCompositeRouter = verifiableProperties.getBoolean(ENABLE_COMPOSITE_ROUTER, false);
-    restServerPipedAsyncChannelSecondaryTimeoutMs =
-        verifiableProperties.getInt(PIPED_ASYNC_CHANNEL_SECONDARY_TIMEOUT_MS, 100);
   }
 }
