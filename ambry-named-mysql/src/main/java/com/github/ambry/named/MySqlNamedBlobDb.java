a1645735d1a2da2e594fedebb729a58f3111c089
Sanketh Nalli <snalli@linkedin.com>
2024-12-03 06:22:12 -0800

    use CTE

--- ambry-named-mysql/src/main/java/com/github/ambry/named/MySqlNamedBlobDb.java
+++ ambry-named-mysql/src/main/java/com/github/ambry/named/MySqlNamedBlobDb.java
@@ -112,18 +112,22 @@ class MySqlNamedBlobDb implements NamedBlobDb {
    *    with filter on the blob_name, and order by blob_name.
    */
   // @formatter:off
-  private static final String LIST_QUERY_V2 = String.format(""
-      + "SELECT blob_name, blob_id, MAX(version) AS version, deleted_ts, blob_size, modified_ts"
-      + "  FROM named_blobs_v2"
-      + "  WHERE account_id = ?"
-      + "    AND container_id = ?"
-      + "    AND blob_state = %1$s"
-      + "    AND (deleted_ts IS NULL OR deleted_ts > %2$S)"
-      + "    AND blob_name LIKE ?"
-      + "    AND blob_name >= ?"
-      + "  GROUP BY blob_name"
-      + "  ORDER BY blob_name ASC"
-      + "  LIMIT ?;", STATE_MATCH, CURRENT_TIME);
+  private static final String LIST_QUERY_V2 = String.format("\n"
+      + "WITH BlobVersions AS (\n"
+      + "  SELECT blob_name, blob_id, MAX(version) AS version, deleted_ts, blob_size, modified_ts\n"
+      + "  FROM named_blobs_v2\n"
+      + "  WHERE account_id = ?\n"
+      + "    AND container_id = ?\n"
+      + "    AND blob_state = %1$s\n"
+      + "    AND blob_name LIKE ?\n"
+      + "    AND blob_name >= ?\n"
+      + "  GROUP BY blob_name\n"
+      + ")\n"
+      + "SELECT *\n"
+      + "FROM BlobVersions\n"
+      + "WHERE (deleted_ts IS NULL OR deleted_ts>%2$S)\n"
+      + "ORDER BY blob_name ASC\n"
+      + "LIMIT ?;", STATE_MATCH, CURRENT_TIME);
   // @formatter:on
 
   /**
