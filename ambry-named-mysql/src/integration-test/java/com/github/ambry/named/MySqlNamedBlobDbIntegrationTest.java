a4da8c5fafb197dbef6d62728d3258e3c5f09b48
Sanketh Nalli <snalli@linkedin.com>
2024-12-03 16:40:07 -0800

    Revert "use getBlobId; strange corupption"
    This reverts commit 1a0b9668a832a6e9c5ed426fdc80702938788343.

--- ambry-named-mysql/src/integration-test/java/com/github/ambry/named/MySqlNamedBlobDbIntegrationTest.java
+++ ambry-named-mysql/src/integration-test/java/com/github/ambry/named/MySqlNamedBlobDbIntegrationTest.java
@@ -279,9 +279,9 @@ public class MySqlNamedBlobDbIntegrationTest {
     Page<NamedBlobRecord> page;
 
     // put blob Ready and list should return the blob
-    v1 = new NamedBlobRecord(account.getName(), container.getName(), blobName, getBlobId(account, container),
+    v1 = new NamedBlobRecord(account.getName(), container.getName(), blobName, "b1-ready",
         now + TimeUnit.MINUTES.toMillis(5));
-    v1_other = new NamedBlobRecord(account.getName(), container.getName(), blobName + "-other", getBlobId(account, container),
+    v1_other = new NamedBlobRecord(account.getName(), container.getName(), blobName + "-other", "b1-ready-other",
         now + TimeUnit.MINUTES.toMillis(5));
     namedBlobDb.put(v1, NamedBlobState.READY, true).get();
     namedBlobDb.put(v1_other, NamedBlobState.READY, true).get();
@@ -292,7 +292,7 @@ public class MySqlNamedBlobDbIntegrationTest {
     time.sleep(100);
 
     // put blob in-progress and list should return the Ready blob
-    v2 = new NamedBlobRecord(account.getName(), container.getName(), blobName, getBlobId(account, container),
+    v2 = new NamedBlobRecord(account.getName(), container.getName(), blobName, "b2-in-progress",
         now + TimeUnit.MINUTES.toMillis(5));
     namedBlobDb.put(v2, NamedBlobState.IN_PROGRESS, true).get();
     page = namedBlobDb.list(account.getName(), container.getName(), blobName, null, null).get();
@@ -302,9 +302,9 @@ public class MySqlNamedBlobDbIntegrationTest {
     time.sleep(100);
 
     // update blob and list should return the new blob
-    v2 = new NamedBlobRecord(account.getName(), container.getName(), blobName, getBlobId(account, container),
+    v2 = new NamedBlobRecord(account.getName(), container.getName(), blobName, "b2-ready",
         now + TimeUnit.MINUTES.toMillis(5));
-    v2_other = new NamedBlobRecord(account.getName(), container.getName(), blobName + "-other", getBlobId(account, container),
+    v2_other = new NamedBlobRecord(account.getName(), container.getName(), blobName + "-other", "b2-ready-other",
         now + TimeUnit.MINUTES.toMillis(5));
     namedBlobDb.put(v2, NamedBlobState.READY, true).get();
     namedBlobDb.put(v2_other, NamedBlobState.READY, true).get();
