4f2856637f67cb9fc7c710640214a70c893ef739
aga9900 <suragarw@linkedin.com>
2024-12-17 01:14:44 +0530

    Updating Copyright Details and Removing commented code

--- ambry-file-transfer/src/main/java/com/github/ambry/filetransfer/FileBasedReplicationManager.java
+++ ambry-file-transfer/src/main/java/com/github/ambry/filetransfer/FileBasedReplicationManager.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2016 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
 package com.github.ambry.filetransfer;
 
 import com.codahale.metrics.MetricRegistry;
@@ -22,8 +35,6 @@ public class FileBasedReplicationManager {
   protected final Logger logger = LoggerFactory.getLogger(getClass());
   protected final PrioritizationManager prioritizationManager;
 
-  //private final FileCopyController fileCopyController;
-
   private final StoreManager storeManager;
 
   public FileBasedReplicationManager(PrioritizationManager prioritizationManager, FileCopyBasedReplicationConfig fileCopyBasedReplicationConfig, ClusterMapConfig clusterMapConfig,
@@ -40,8 +51,6 @@ public class FileBasedReplicationManager {
       prioritizationManager.start();
     }
 
-//    fileCopyController = new FileCopyController(prioritizationManager, storeManager);
-//    fileCopyController.start();
     this.storeManager = storeManager;
   }
   public void start() throws InterruptedException, IOException {
