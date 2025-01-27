c3599e74b6737dd695c03b4315bea2aaa5f58aaf
Navneet Sachan <nsachan@linkedin.com>
2025-01-27 12:41:56 +0530

    add put load producer

--- ambry-tools/src/main/java/com/github/ambry/tools/perf/serverperf/PutLoadProducerConsumer.java
+++ ambry-tools/src/main/java/com/github/ambry/tools/perf/serverperf/PutLoadProducerConsumer.java
@@ -1,18 +1,36 @@
 package com.github.ambry.tools.perf.serverperf;
 
 import com.github.ambry.clustermap.ClusterMap;
+import com.github.ambry.clustermap.DataNodeId;
+import com.github.ambry.clustermap.ReplicaId;
 import com.github.ambry.tools.perf.serverperf.ServerPerformance.ServerPerformanceConfig;
+import java.util.List;
 
 
 public class PutLoadProducerConsumer implements LoadProducerConsumer {
+  private final ServerPerfNetworkQueue networkQueue;
+  private final ServerPerformanceConfig config;
+  private final ClusterMap clusterMap;
 
   public PutLoadProducerConsumer(ServerPerfNetworkQueue networkQueue, ServerPerformanceConfig config,
       ClusterMap clusterMap) {
+    this.networkQueue = networkQueue;
+    this.config = config;
+    this.clusterMap = clusterMap;
   }
 
   @Override
   public void produce() throws Exception {
+    DataNodeId dataNodeId = clusterMap.getDataNodeId(config.serverPerformanceHostname, config.serverPerformancePort);
+    List<? extends ReplicaId> replicaIds = clusterMap.getReplicaIds(dataNodeId);
 
+    replicaIds.forEach(replicaId -> {
+      if (!replicaId.isUnsealed()) {
+        return;
+      }
+
+      //    PutRequest putRequest = new PutRequest(1, BlobId.BlobIdType.NATIVE, clusterMap.getLocalDatacenterId(), )
+    });
   }
 
   @Override
