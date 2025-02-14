862080b7e66aeced53515311b48841fd56d95a73
Justin Lin <juslin@linkedin.com>
2025-02-13 18:46:03 -0800

    Fix typo

--- ambry-clustermap/src/main/java/com/github/ambry/clustermap/HelixClusterManagerMetrics.java
+++ ambry-clustermap/src/main/java/com/github/ambry/clustermap/HelixClusterManagerMetrics.java
@@ -181,7 +181,7 @@ class HelixClusterManagerMetrics {
 
     Gauge<Long> onHostPartitionSealedCount = clusterMapCallback::getOnHostPartitionSealedCount;
     registry.gauge(MetricRegistry.name(HelixClusterManager.class, "onHostPartitionSealedCount"),
-        () -> );
+        () -> onHostPartitionSealedCount);
 
     Gauge<Long> partitionPartiallySealedCount = clusterMapCallback::getPartitionPartiallySealedCount;
     registry.gauge(MetricRegistry.name(HelixClusterManager.class, "partitionPartiallySealedCount"),
