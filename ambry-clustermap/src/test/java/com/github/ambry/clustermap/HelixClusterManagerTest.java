a7ee13e449385be9a84358eea1481305d78129b3
Mudit Saxena <muditsaxena0862@gmail.com>
2025-03-26 11:23:07 +0530

    Added a util method to fetch MIN_ACTIVE_REPLICA from helix (#3044)
    * Added a util method to fetch MIN_ACTIVE_REPLICA from helix
    
    * Removed default value
    
    * Refactored method signature to have PartitionId

--- ambry-clustermap/src/test/java/com/github/ambry/clustermap/HelixClusterManagerTest.java
+++ ambry-clustermap/src/test/java/com/github/ambry/clustermap/HelixClusterManagerTest.java
@@ -2178,6 +2178,28 @@ public class HelixClusterManagerTest {
     }
   }
 
+
+  @Test
+  public void helixClusterGetMinActiveReplicas() throws Exception {
+    assumeTrue(!useComposite && !overrideEnabled);
+    HelixClusterManager helixClusterManager = (HelixClusterManager) clusterManager;
+    HelixClusterManager.HelixClusterManagerQueryHelper clusterHelper = helixClusterManager.new HelixClusterManagerQueryHelper();
+    verifyInitialClusterChanges(helixClusterManager, helixCluster, new String[]{localDc});
+
+
+    PartitionId partitionId = clusterHelper.getPartitions().iterator().next();
+    List<String> resourceNames = helixCluster.getResources(localDc);
+    String resourceName = resourceNames.get(0);
+    String tag = "TAG_100000";
+    IdealState idealState = helixCluster.getResourceIdealState(resourceName, localDc);
+    idealState.setInstanceGroupTag(tag);
+    idealState.setMinActiveReplicas(2);
+    helixCluster.refreshIdealState();
+
+    int minActiveReplicaConfig = clusterHelper.getMinActiveReplicas(partitionId);
+    assertEquals(idealState.getMinActiveReplicas(), minActiveReplicaConfig);
+  }
+
   // Helpers
 
   /**
