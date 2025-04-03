68ebb96e9c8c307a147e106f21ce7a0784b7e5ee
CH Chethan Reddy <chereddy@linkedin.com>
2025-04-03 13:08:32 +0530

    Add interfaces for Prioritization Manager (#3050)
    * Add interfaces for Prioritization Manager
    
    * Address comments.
    
    * Add Licensing.

---
+++ ambry-prioritization/src/main/java/com/github/ambry/replica/prioritization/DefaultDisruptionService.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2025 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+package com.github.ambry.replica.prioritization;
+
+import com.github.ambry.clustermap.PartitionId;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DefaultDisruptionService implements DisruptionService {
+  // This class is a placeholder for the actual implementation of the DisruptionService.
+  // It should contain methods to handle disruptions and batch operations by partition.
+
+  @Override
+  public Map<PartitionId, List<Operation>> batchDisruptionsByPartition(List<PartitionId> partitionIds) {
+    Map<PartitionId, List<Operation>> partitionOperationsMap = new HashMap<>();
+    for (PartitionId partitionId : partitionIds) {
+      // Process each partitionId
+      partitionOperationsMap.put(partitionId, Arrays.asList());
+    }
+
+    return partitionOperationsMap;
+  }
+}
