ad34cfa3f07e7bc43107809874522bd3c923f53d
Danny Cheng <dacheng@dacheng-mn2116.linkedin.biz>
2025-04-14 10:29:27 -0700

    Implement InMemoryAccountService
    This implementation of the AccountService enables the /accounts
    API to work when deploying Ambry locally without any external dependencies.
    Note that this implementation is primarily meant for local testing
    and not for production use.
    
    The InMemoryAccountService essentially combines the functionality of the previous
    InMemoryUnknownAccountService and JsonAccountService. However, since
    the /accounts API works, it does not depend on continuously reading a
    JSON file on disk to fetch account info (but does accept one on startup
    to quickly fill in data).
    
    Delete the InMemoryUnknownAccountService and JsonAccountService
    since they are no longer needed with this implementation.

---
+++ ambry-account/src/main/java/com/github/ambry/account/InMemoryAccountService.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2025 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package com.github.ambry.account;
+
+import com.github.ambry.config.AccountServiceConfig;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Account service which holds account data in-memory.
+ *
+ * This implementation of the account service is meant primarily for local testing and deployment.
+ */
+public final class InMemoryAccountService extends AbstractAccountService {
+
+  static Account UNKNOWN_ACCOUNT = new Account(Account.UNKNOWN_ACCOUNT_ID, Account.UNKNOWN_ACCOUNT_NAME, Account.AccountStatus.ACTIVE,
+      Account.ACL_INHERITED_BY_CONTAINER_DEFAULT_VALUE, Account.SNAPSHOT_VERSION_DEFAULT_VALUE,
+      Arrays.asList(Container.UNKNOWN_CONTAINER, Container.DEFAULT_PUBLIC_CONTAINER,
+          Container.DEFAULT_PRIVATE_CONTAINER), Account.QUOTA_RESOURCE_TYPE_DEFAULT_VALUE);
+  private static final Logger logger = LoggerFactory.getLogger(InMemoryAccountService.class);
+
+  /** Location of the account file which this service uses as it's source of accounts.*/
+  private final Path accountFile;
+
+  /**
+   * Constructs an {@code InMemoryAccountService}. {@code #init} method MUST be called after construction.
+   *
+   * @param accountServiceMetrics Metrics instance to log metrics to.
+   */
+  public InMemoryAccountService(Path accountFile, AccountServiceMetrics accountServiceMetrics, AccountServiceConfig accountConfig) {
+    super(accountConfig, accountServiceMetrics, null);
+    this.accountFile = accountFile;
+  }
+
+  /** Guards against calling {@link #init} multiple times.*/
+  private final AtomicBoolean initialized = new AtomicBoolean();
+
+  /**
+   * Initializes this instance. Does all work which requires handing out references to this instance itself which can't
+   * be done during normal instance construction without letting a reference to an incomplete constructed instance escape.
+   */
+  public void init() {
+    if (!initialized.getAndSet(true)) {
+      processAccountJsonFile();
+
+      // add the default unknown account
+      ArrayList<Account> accounts = new ArrayList<Account>();
+      accounts.add(UNKNOWN_ACCOUNT);
+
+      try {
+        updateAccounts(accounts);
+      } catch (AccountServiceException e) {
+        logger.warn("Unable to add default unknown account", e);
+      }
+
+      }
+  }
+
+  @Override
+  protected void checkOpen() {
+    if (!initialized.get()) {
+      throw new IllegalStateException("AccountService is closed.");
+    }
+  }
+
+  @Override
+  protected void onAccountChangeMessage(String topic, String message) {
+    // We're good.
+  }
+
+  /**
+   * Adds accounts
+   *
+   * @param accounts The collection of {@link Account}s to update. Cannot be {@code null}.
+   */
+  @Override
+  public void updateAccounts(Collection<Account> accounts) throws AccountServiceException {
+    if (accountInfoMapRef.get().hasConflictingAccount(accounts, true)) {
+      throw new AccountServiceException("Input accounts conflict with the accounts in local cache",
+          AccountServiceErrorCode.ResourceConflict);
+    }
+    for (Account account : accounts) {
+      if (accountInfoMapRef.get().hasConflictingContainer(account.getAllContainers(), account.getId(), true)) {
+        throw new AccountServiceException("Containers in account " + account.getId() + " conflict with local cache",
+            AccountServiceErrorCode.ResourceConflict);
+      }
+    }
+    accountInfoMapRef.get().addOrUpdateAccounts(accounts);
+  }
+
+  @Override
+  public void close() throws IOException {
+  }
+
+  /**
+   * Loads initial accounts into memory via a JSON file.
+   */
+  private void processAccountJsonFile() {
+    if (accountFile == null) {
+      return;
+    }
+
+    try {
+      if (!Files.exists(accountFile)) {
+        logger.warn("JSON account file '{}' does not exist", accountFile.toAbsolutePath());
+        return;
+      }
+      String accountsJsonString = new String(Files.readAllBytes(accountFile), StandardCharsets.UTF_8);
+      AccountInfoMap newAccountInfoMap = new AccountInfoMap(accountsJsonString);
+      AccountInfoMap oldAccountInfoMap = accountInfoMapRef.getAndSet(newAccountInfoMap);
+
+      //Notify modified accounts to consumers
+      Collection<Account> updatedAccounts = newAccountInfoMap.getAccounts()
+          .stream()
+          .filter(newAccount -> !newAccount.equals(oldAccountInfoMap.getAccountById(newAccount.getId())))
+          .collect(Collectors.toSet());
+      notifyAccountUpdateConsumers(updatedAccounts, false);
+
+      logger.debug("Finished updating account maps");
+
+    } catch (Exception e) {
+      logger.error("Failed updating accounts", e);
+    }
+  }
+}
