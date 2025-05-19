f03d667e44f901b9569b683981b190cf4fd1ec64
Navneet Sachan <nsachan@linkedin.com>
2025-05-19 14:29:55 +0530

    adding licesnse in test

--- ambry-prioritization/src/test/java/com/github/ambry/replica/prioritization/FileCopyPrioritizationManagerTest.java
+++ ambry-prioritization/src/test/java/com/github/ambry/replica/prioritization/FileCopyPrioritizationManagerTest.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright 2025 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 package com.github.ambry.replica.prioritization;
 
 import static org.junit.Assert.*;
@@ -176,5 +190,8 @@ public class FileCopyPrioritizationManagerTest {
     assertEquals("First replica should match the order from disruptionService", replica2, sortedReplicas.get(0));
     assertEquals("Second replica should match the order from disruptionService", replica1, sortedReplicas.get(1));
     assertEquals("Third replica should match the order from disruptionService", replica3, sortedReplicas.get(2));
+
+    sortedReplicas = prioritizationManager.getPartitionListForDisk(disk, 3);
+    assertEquals("No replicas should be polled now", 0, sortedReplicas.size());
   }
 }
